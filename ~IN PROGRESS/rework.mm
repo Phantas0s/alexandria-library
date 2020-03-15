35873ebd013839b6a0a0941aab568766b192e405
matthieu <matthieu.cneude@gmail.com>
2020-03-15 06:05:29 +0100

    Add rework

---
+++ ~IN PROGRESS/rework.mm
@@ -0,0 +1,317 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1584081890716" ID="ID_488848990" MODIFIED="1584081955775" TEXT="Rework">
+<node CREATED="1584082115051" ID="ID_649346224" MODIFIED="1584082137222" POSITION="right" TEXT="Introduction">
+<icon BUILTIN="full-0"/>
+</node>
+<node CREATED="1584082119623" ID="ID_1495884485" MODIFIED="1584082132983" POSITION="right" TEXT="Chapter First">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1584082151160" ID="ID_1419329597" MODIFIED="1584082163317" TEXT="The new reality"/>
+</node>
+<node CREATED="1584082165486" ID="ID_1517634523" MODIFIED="1584082178493" POSITION="right" TEXT="Chapter Takedowns">
+<icon BUILTIN="full-2"/>
+<node CREATED="1584082212052" ID="ID_219480344" MODIFIED="1584083224756" TEXT="Ignore the real world">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1584082249523" ID="ID_1832449866" MODIFIED="1584083225675" TEXT="Learning from mistakes is overrated">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1584082264178" ID="ID_1324030629" MODIFIED="1584083226537" TEXT="Planning is guessing">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1584082281149" ID="ID_312531353" MODIFIED="1584083227305" TEXT="Why grow?">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1584082287960" ID="ID_624002020" MODIFIED="1584083228658" TEXT="Workaholism">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1584082311402" ID="ID_18747181" MODIFIED="1584083230130" TEXT="Enough with &quot;entrepreneurs&quot;">
+<icon BUILTIN="full-6"/>
+</node>
+</node>
+<node CREATED="1584082184027" ID="ID_72156632" MODIFIED="1584082189056" POSITION="right" TEXT="Chapter Go">
+<icon BUILTIN="full-3"/>
+<node CREATED="1584082348324" ID="ID_236531934" MODIFIED="1584083231356" TEXT="Make a dent in the universe">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1584082408768" ID="ID_787096283" MODIFIED="1584083232157" TEXT="Scratch your own itch">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1584082416613" ID="ID_291081530" MODIFIED="1584083233237" TEXT="Start making something">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1584082422499" ID="ID_210238506" MODIFIED="1584083234092" TEXT="No time is no excuse">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1584082487313" ID="ID_1893172581" MODIFIED="1584083234982" TEXT="Draw a line in the sand">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1584082527167" ID="ID_1650438634" MODIFIED="1584083236684" TEXT="Mission statement impossible">
+<icon BUILTIN="full-6"/>
+</node>
+<node CREATED="1584082533131" ID="ID_1588390456" MODIFIED="1584083239282" TEXT="Outside money is Plan Z">
+<icon BUILTIN="full-7"/>
+</node>
+<node CREATED="1584082540962" ID="ID_1395073872" MODIFIED="1584083240777" TEXT="You need less than you think">
+<icon BUILTIN="full-8"/>
+</node>
+<node CREATED="1584082546346" ID="ID_785630897" MODIFIED="1584083242137" TEXT="Start a business, not a startup">
+<icon BUILTIN="full-9"/>
+</node>
+<node CREATED="1584082554446" ID="ID_1122682839" MODIFIED="1584083244807" TEXT="Building to flip is building to flop">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-0"/>
+</node>
+<node CREATED="1584082562376" ID="ID_1733526479" MODIFIED="1584083246675" TEXT="Less mass">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-1"/>
+</node>
+</node>
+<node CREATED="1584082567819" ID="ID_1190004427" MODIFIED="1584082578185" POSITION="right" TEXT="Chapter Progress">
+<icon BUILTIN="full-4"/>
+<node CREATED="1584082583187" ID="ID_226484006" MODIFIED="1584083248836" TEXT="Embrace constraints">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1584082586379" ID="ID_1736478195" MODIFIED="1584083249674" TEXT="Build half a product, not a half-assed product">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1584082597937" ID="ID_417065078" MODIFIED="1584083250554" TEXT="Start at the epicenter">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1584082603010" ID="ID_783776955" MODIFIED="1584083251366" TEXT="Ignore the details early on">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1584082610167" ID="ID_1731836360" MODIFIED="1584083252356" TEXT="Making the call is making progress">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1584082614505" ID="ID_433443306" MODIFIED="1584083253640" TEXT="Be a curator">
+<icon BUILTIN="full-6"/>
+</node>
+<node CREATED="1584082620270" ID="ID_574380504" MODIFIED="1584083254912" TEXT="Throw less at the problem">
+<icon BUILTIN="full-7"/>
+</node>
+<node CREATED="1584082706209" ID="ID_1637901181" MODIFIED="1584083256295" TEXT="Focus on what won&apos;t change">
+<icon BUILTIN="full-8"/>
+</node>
+<node CREATED="1584082710836" ID="ID_1319316209" MODIFIED="1584083260343" TEXT="Tone is in your fingers">
+<icon BUILTIN="full-9"/>
+</node>
+<node CREATED="1584082715277" ID="ID_284318640" MODIFIED="1584083262312" TEXT="Sell your by-products">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-0"/>
+</node>
+<node CREATED="1584082719611" ID="ID_1009681381" MODIFIED="1584083264051" TEXT="Launch now">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-1"/>
+</node>
+</node>
+<node CREATED="1584082723814" ID="ID_754626625" MODIFIED="1584082755172" POSITION="right" TEXT="Chapter Productivity">
+<icon BUILTIN="full-5"/>
+<node CREATED="1584082757047" ID="ID_1429079948" MODIFIED="1584083265644" TEXT="Illusions of agreement">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1584082762567" ID="ID_318312142" MODIFIED="1584083266349" TEXT="Reasons to quit">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1584082766752" ID="ID_768679194" MODIFIED="1584083267153" TEXT="Interruption is the enemy of productivity">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1584082775968" ID="ID_273692151" MODIFIED="1584083268069" TEXT="Meetings are toxic">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1584082780435" ID="ID_540577273" MODIFIED="1584083268839" TEXT="Good enough is fine">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1584082784576" ID="ID_1447230006" MODIFIED="1584083270042" TEXT="Quick wins">
+<icon BUILTIN="full-6"/>
+</node>
+<node CREATED="1584082788503" ID="ID_835195919" MODIFIED="1584083272983" TEXT="Don&apos;t be a hero">
+<icon BUILTIN="full-7"/>
+</node>
+<node CREATED="1584082797036" ID="ID_1396214876" MODIFIED="1584083274276" TEXT="Go to sleep">
+<icon BUILTIN="full-8"/>
+</node>
+<node CREATED="1584082798953" ID="ID_1900807271" MODIFIED="1584083276944" TEXT="Your estimates suck">
+<icon BUILTIN="full-9"/>
+</node>
+<node CREATED="1584082803936" ID="ID_1335450" MODIFIED="1584083278935" TEXT="Long lists don&apos;t get done">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-0"/>
+</node>
+<node CREATED="1584082809915" ID="ID_555148954" MODIFIED="1584083280284" TEXT="Make tiny decisions">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-1"/>
+</node>
+</node>
+<node CREATED="1584082817276" ID="ID_788970696" MODIFIED="1584082834311" POSITION="right" TEXT="Chapter Competitors">
+<icon BUILTIN="full-6"/>
+<node CREATED="1584082836720" ID="ID_94535353" MODIFIED="1584083282148" TEXT="Don&apos;t copy">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1584082838810" ID="ID_1550053938" MODIFIED="1584083282917" TEXT="Decommoditize your product">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1584082854599" ID="ID_1481953423" MODIFIED="1584083283797" TEXT="Pick a fight">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1584082859469" ID="ID_758110407" MODIFIED="1584083284540" TEXT="Underdo your competition">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1584082864506" ID="ID_1762804960" MODIFIED="1584083285365" TEXT="Who cares what they&apos;re doing?">
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+<node CREATED="1584082875611" ID="ID_1426927397" MODIFIED="1584082889332" POSITION="left" TEXT="Chapter Evolution">
+<icon BUILTIN="full-7"/>
+<node CREATED="1584082895808" ID="ID_1556375814" MODIFIED="1584083297403" TEXT="Say no by default">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1584082901983" ID="ID_982192781" MODIFIED="1584083298235" TEXT="Let your customers outgrow you">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1584082907976" ID="ID_269453892" MODIFIED="1584083298915" TEXT="Don&apos;t confuse enthusiasm with priority">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1584082920037" ID="ID_127775680" MODIFIED="1584083299767" TEXT="Be at-home good">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1584082926896" ID="ID_822597760" MODIFIED="1584083300524" TEXT="Don&apos;t write it down">
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+<node CREATED="1584082931807" ID="ID_1158369587" MODIFIED="1584082935676" POSITION="left" TEXT="Chapter Promotion">
+<icon BUILTIN="full-8"/>
+<node CREATED="1584082937907" ID="ID_930366228" MODIFIED="1584083301712" TEXT="Welcome obscurity">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1584082942212" ID="ID_1136579792" MODIFIED="1584083302493" TEXT="Build an audience">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1584082949511" ID="ID_566674309" MODIFIED="1584083303281" TEXT="Out-teach your competition">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1584082958401" ID="ID_1055571943" MODIFIED="1584083304286" TEXT="Emulate chefs">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1584082962102" ID="ID_1351750770" MODIFIED="1584083305107" TEXT="Go behind the scenes">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1584082969090" ID="ID_452228960" MODIFIED="1584083306735" TEXT="Nobody likes plastic flowers">
+<icon BUILTIN="full-6"/>
+</node>
+<node CREATED="1584082976398" ID="ID_149692859" MODIFIED="1584083307871" TEXT="Press releases are spam">
+<icon BUILTIN="full-7"/>
+</node>
+<node CREATED="1584082982290" ID="ID_1222539137" MODIFIED="1584083308820" TEXT="Forget about the Wall Street Journal">
+<icon BUILTIN="full-8"/>
+</node>
+<node CREATED="1584082993255" ID="ID_821423134" MODIFIED="1584083310571" TEXT="Drug dealers get it right">
+<icon BUILTIN="full-9"/>
+</node>
+<node CREATED="1584082999085" ID="ID_954212965" MODIFIED="1584083313440" TEXT="Marketing is not a department">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-0"/>
+</node>
+<node CREATED="1584083005635" ID="ID_995699307" MODIFIED="1584083315038" TEXT="The myth of the overnight sensation">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-1"/>
+</node>
+</node>
+<node CREATED="1584083016303" ID="ID_585938949" MODIFIED="1584083022607" POSITION="left" TEXT="Chapter Hiring">
+<icon BUILTIN="full-9"/>
+<node CREATED="1584083024811" ID="ID_752922792" MODIFIED="1584083316488" TEXT="Do it yourself first">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1584083028033" ID="ID_1516594205" MODIFIED="1584083317389" TEXT="Hire when it hurts">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1584083032033" ID="ID_730056120" MODIFIED="1584083318192" TEXT="Pass on great people">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1584083036288" ID="ID_1998638560" MODIFIED="1584083318996" TEXT="Strangers at a cocktail party">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1584083047122" ID="ID_142006765" MODIFIED="1584083319756" TEXT="Resum&#xe9;s are ridiculous">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1584083054227" ID="ID_1073452159" MODIFIED="1584083321296" TEXT="Years of irrelevance">
+<icon BUILTIN="full-6"/>
+</node>
+<node CREATED="1584083078773" ID="ID_1220237356" MODIFIED="1584083324326" TEXT="Forget about formal education">
+<icon BUILTIN="full-7"/>
+</node>
+<node CREATED="1584083084767" ID="ID_1274988651" MODIFIED="1584083326306" TEXT="Everybody works">
+<icon BUILTIN="full-8"/>
+</node>
+<node CREATED="1584083088543" ID="ID_1742079948" MODIFIED="1584083330047" TEXT="Hire managers of one">
+<icon BUILTIN="full-9"/>
+</node>
+<node CREATED="1584083093569" ID="ID_598594943" MODIFIED="1584083332893" TEXT="Hire great writers">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-0"/>
+</node>
+<node CREATED="1584083096663" ID="ID_1912207473" MODIFIED="1584083334375" TEXT="The best are everywhere">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1584083101549" ID="ID_993211311" MODIFIED="1584083336640" TEXT="Test-drive employees">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-2"/>
+</node>
+</node>
+<node CREATED="1584083109150" ID="ID_1701115677" MODIFIED="1584083116242" POSITION="left" TEXT="Chapter Damage Control">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-0"/>
+<node CREATED="1584083119572" ID="ID_1877506550" MODIFIED="1584083339454" TEXT="Own your bad news">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1584083123838" ID="ID_1405489336" MODIFIED="1584083340266" TEXT="Speed changes everything">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1584083127530" ID="ID_175623816" MODIFIED="1584083341066" TEXT="How to say you&apos;re sorry">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1584083132318" ID="ID_293696501" MODIFIED="1584083341760" TEXT="Put everyone on the front lines">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1584083143783" ID="ID_612730158" MODIFIED="1584083342657" TEXT="Take a deep breath">
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+<node CREATED="1584083151281" ID="ID_975216034" MODIFIED="1584083207834" POSITION="left" TEXT="Chapter Culture">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1584083153689" ID="ID_249776084" MODIFIED="1584083344166" TEXT="You don&apos;t create a culture">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1584083157746" ID="ID_124154546" MODIFIED="1584083345019" TEXT="Decisions are temporary">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1584083162593" ID="ID_632467392" MODIFIED="1584083345734" TEXT="Skip the rock stars">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1584083171868" ID="ID_219523342" MODIFIED="1584083346517" TEXT="They&apos;re not thirteen">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1584083175616" ID="ID_294492651" MODIFIED="1584083348000" TEXT="Send people home at 5">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1584083180679" ID="ID_667548027" MODIFIED="1584083349196" TEXT="Don&apos;t scar on the first cut">
+<icon BUILTIN="full-6"/>
+</node>
+<node CREATED="1584083185618" ID="ID_524374052" MODIFIED="1584083351016" TEXT="Sound like you">
+<icon BUILTIN="full-7"/>
+</node>
+<node CREATED="1584083188863" ID="ID_1963401909" MODIFIED="1584083352607" TEXT="Four-letter words">
+<icon BUILTIN="full-8"/>
+</node>
+<node CREATED="1584083196091" ID="ID_173589275" MODIFIED="1584083353913" TEXT="ASAP is poison">
+<icon BUILTIN="full-9"/>
+</node>
+</node>
+<node CREATED="1584083202766" ID="ID_1051104297" MODIFIED="1584083205133" POSITION="left" TEXT="Conclusion">
+<node CREATED="1584083211661" ID="ID_852456926" MODIFIED="1584083216155" TEXT="Inspiration is perishable"/>
+</node>
+</node>
+</map>
