8476965b4ec58cf85762a837fa009174c833b276
matthieu <matthieu.cneude@gmail.com>
2020-03-30 17:56:51 +0200

    Do some search on finances

--- finances/investment.mm
+++ finances/investment.mm
@@ -51,7 +51,7 @@
 <node CREATED="1547357323784" ID="ID_563058208" MODIFIED="1547357325843" TEXT="Less risky"/>
 </node>
 </node>
-<node CREATED="1547357330619" ID="ID_252233185" MODIFIED="1547357335382" TEXT="Need a courtier?"/>
+<node CREATED="1547357330619" ID="ID_252233185" MODIFIED="1585505833587" TEXT="Need a broker"/>
 <node CREATED="1547357374758" ID="ID_51757057" MODIFIED="1547357381620" TEXT="Can be choose depending on">
 <node CREATED="1547357381808" ID="ID_200154378" MODIFIED="1547357385355" TEXT="Geographic area"/>
 <node CREATED="1547357385888" ID="ID_1729744767" MODIFIED="1547357390733" TEXT="Activity"/>
@@ -61,6 +61,37 @@
 </node>
 <node CREATED="1547358113017" ID="ID_614309444" MODIFIED="1547358117751" TEXT="Liquidity level">
 <node CREATED="1547358117962" ID="ID_734813670" MODIFIED="1547358130309" TEXT="Difference between selling and buying price"/>
+</node>
+<node CREATED="1585506134586" ID="ID_899778871" LINK="https://www.investopedia.com/articles/exchangetradedfunds/08/etf-choose-best.asp" MODIFIED="1585506149457" TEXT="Picking the right ETF">
+<node CREATED="1585506158952" ID="ID_1833223566" MODIFIED="1585506198382" TEXT="Level of Assets">
+<icon BUILTIN="full-1"/>
+<node CREATED="1585506160905" ID="ID_365429901" MODIFIED="1585506171044" TEXT="At least threshold of 10 millions"/>
+</node>
+<node CREATED="1585506201315" ID="ID_1905986984" MODIFIED="1585506541987" TEXT="Trading volume">
+<icon BUILTIN="full-2"/>
+<node CREATED="1585506212201" ID="ID_1673590157" MODIFIED="1585506218680" TEXT="Need sufficient volume trade on daily basis">
+<node CREATED="1585506223583" ID="ID_796043639" MODIFIED="1585506247672" TEXT="Most popular: million of daily shares"/>
+</node>
+<node CREATED="1585506267398" ID="ID_1313888997" MODIFIED="1585506272106" TEXT="Pretty important when quitting ETF"/>
+</node>
+<node CREATED="1585506277289" ID="ID_760447324" MODIFIED="1585506542951" TEXT="Underlying index or asset">
+<icon BUILTIN="full-3"/>
+<node CREATED="1585506322948" ID="ID_77044951" LINK="https://www.investopedia.com/terms/i/index.asp" MODIFIED="1585506424606" TEXT="Index definition">
+<node CREATED="1585506325049" ID="ID_1793485710" MODIFIED="1585506332182" TEXT="Represent security of an ETF"/>
+<node CREATED="1585506332650" ID="ID_82250160" MODIFIED="1585506345801" TEXT="Each index has its own calculation"/>
+<node CREATED="1585506357676" ID="ID_1800936657" MODIFIED="1585506363163" TEXT="Relative change more important"/>
+</node>
+<node CREATED="1585506436817" ID="ID_813992925" MODIFIED="1585506449252" TEXT="Better if based on broad, widely available index"/>
+</node>
+<node CREATED="1585506508500" ID="ID_1165664777" MODIFIED="1585506543787" TEXT="Tracking error">
+<icon BUILTIN="full-4"/>
+<node CREATED="1585506512233" ID="ID_259454083" MODIFIED="1585506520559" TEXT="Some ETF don&apos;t track index closely enough"/>
+</node>
+<node CREATED="1585506546957" ID="ID_1924116666" MODIFIED="1585506557717" TEXT="Market position">
+<icon BUILTIN="full-5"/>
+<node CREATED="1585506568594" ID="ID_833644685" MODIFIED="1585506581849" TEXT="Avoid ETF which imitated an original idea"/>
+<node CREATED="1585506586308" ID="ID_1363705766" MODIFIED="1585506596796" TEXT="Don&apos;t differentiate themselves enough"/>
+</node>
 </node>
 </node>
 </node>
