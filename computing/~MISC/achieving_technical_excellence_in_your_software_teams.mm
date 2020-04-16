e71f323c0bdf416e1bc5c762f1ccc06e5e6dfcaf
matthieu <matthieu.cneude@gmail.com>
2020-04-16 13:22:46 +0200

    Add talk about many things computing related

---
+++ computing/~MISC/achieving_technical_excellence_in_your_software_teams.mm
@@ -0,0 +1,43 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1586974336414" ID="ID_605190516" LINK="https://www.youtube.com/watch?v=-TTVclZwDZU" MODIFIED="1586974353721" TEXT="Achieving Technical Excellence in Your Software Teams">
+<node CREATED="1586974354546" ID="ID_1349893090" MODIFIED="1586974359809" POSITION="right" TEXT="Continuous Deployment">
+<node CREATED="1586974360478" ID="ID_986854594" MODIFIED="1586974363031" TEXT="Deployed">
+<node CREATED="1586974363262" ID="ID_858614030" MODIFIED="1586974373390" TEXT="Changes introduced in production"/>
+</node>
+<node CREATED="1586974375887" ID="ID_366955522" MODIFIED="1586974377951" TEXT="Released">
+<node CREATED="1586974378189" ID="ID_1111675211" MODIFIED="1586974382356" TEXT="Visible to the end user"/>
+<node CREATED="1586974485124" ID="ID_1248167323" MODIFIED="1586974486132" TEXT="How?">
+<node CREATED="1586974486314" ID="ID_210882922" MODIFIED="1586974488368" TEXT="Feature flags"/>
+<node CREATED="1586974488786" ID="ID_673594579" MODIFIED="1586974491420" TEXT="Permission system">
+<node CREATED="1586974491578" ID="ID_591198429" MODIFIED="1586974500910" TEXT="Only a subset of people"/>
+<node CREATED="1586974501556" ID="ID_1922304998" MODIFIED="1586974504213" TEXT="... see the features"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1586974955069" ID="ID_463347174" MODIFIED="1586975207013" POSITION="right" TEXT="Decision / knowledge">
+<node CREATED="1586975207243" FOLDED="true" ID="ID_764207622" MODIFIED="1586975274508" TEXT="Curve">
+<icon BUILTIN="licq"/>
+<node CREATED="1586975209350" ID="ID_1717064238" MODIFIED="1586975239666">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="decision_knowledge.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1586975250363" ID="ID_1326142403" MODIFIED="1586975253089" TEXT="Problem">
+<node CREATED="1586975254599" ID="ID_1059738082" MODIFIED="1586975261407" TEXT="We take the biggest decisions"/>
+<node CREATED="1586975261796" ID="ID_1736062603" MODIFIED="1586975268487" TEXT="... when we have the least knowledge"/>
+</node>
+<node CREATED="1586975275434" ID="ID_1463589835" MODIFIED="1586975279303" TEXT="Questions to ask">
+<node CREATED="1586975279501" ID="ID_725508176" MODIFIED="1586975285263" TEXT="Do we decide that now?"/>
+<node CREATED="1586975285722" ID="ID_1348185356" MODIFIED="1586975293863" TEXT="How can we gain knowledge quickly?"/>
+</node>
+</node>
+</node>
+</map>
