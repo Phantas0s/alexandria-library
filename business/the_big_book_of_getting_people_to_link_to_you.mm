f3fa62a5bcffe03f03e36c5e0affaf1d53ef12fb
matthieu <matthieu.cneude@gmail.com>
2020-06-06 10:13:28 +0200

    Map article from Patrick McKenzie

---
+++ business/the_big_book_of_getting_people_to_link_to_you.mm
@@ -0,0 +1,69 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1591424993065" ID="ID_1077188038" LINK="https://www.kalzumeus.com/2008/04/08/linkbuilding-for-small-businesses/" MODIFIED="1591425081036" TEXT="The Big Book of Getting People to Link to You">
+<node CREATED="1591425209041" ID="ID_93545303" MODIFIED="1591425240830" POSITION="right" TEXT="Introduction">
+<icon BUILTIN="full-0"/>
+<node CREATED="1591425211512" ID="ID_1279075703" MODIFIED="1591425220669" TEXT="Hard for people to link to you"/>
+<node CREATED="1591425228170" ID="ID_823663652" MODIFIED="1591425233220" TEXT="... especially if you sell something"/>
+</node>
+<node CREATED="1591425202527" ID="ID_668861581" MODIFIED="1591425205369" POSITION="right" TEXT="Make some friends, fans, or fans into friends">
+<icon BUILTIN="full-1"/>
+<node CREATED="1591425303189" ID="ID_868985784" MODIFIED="1591425337968" TEXT="Needs fans">
+<node CREATED="1591425322896" ID="ID_85158819" LINK="https://kk.org/thetechnium/1000-true-fans/" MODIFIED="1591425340924" TEXT="Just need 1000 people"/>
+<node CREATED="1591425327941" ID="ID_174633036" MODIFIED="1591425331580" TEXT="... buying what you sell"/>
+<node CREATED="1591425331984" ID="ID_1561631869" MODIFIED="1591425334148" TEXT="... to make a living"/>
+</node>
+<node CREATED="1591425407391" ID="ID_1473417370" MODIFIED="1591425413587" TEXT="... they&apos;ll speak about you">
+<node CREATED="1591425413773" ID="ID_768606053" MODIFIED="1591425419131" TEXT="Them might have a following"/>
+<node CREATED="1591425419527" ID="ID_1804071586" MODIFIED="1591425422147" TEXT="... or a blog"/>
+</node>
+<node CREATED="1591425467303" ID="ID_1902682642" MODIFIED="1591425471531" TEXT="Engage in your local community"/>
+<node CREATED="1591425486126" ID="ID_382836774" MODIFIED="1591425490418" TEXT="Help others"/>
+</node>
+<node CREATED="1591425491270" ID="ID_1166390676" MODIFIED="1591425571938" POSITION="right" TEXT="Blogging for your customers and people like them">
+<icon BUILTIN="full-2"/>
+<node CREATED="1591425529670" ID="ID_1698255113" MODIFIED="1591425534979" TEXT="Speak directly to your clients"/>
+</node>
+<node CREATED="1591425565733" ID="ID_152265602" MODIFIED="1591425574990" POSITION="right" TEXT="Create resources your customers/people like them can use">
+<icon BUILTIN="full-3"/>
+<node CREATED="1591425647093" ID="ID_1402183400" MODIFIED="1591425651705" TEXT="Create a page useful for many"/>
+<node CREATED="1591425652500" ID="ID_1878225429" MODIFIED="1591425655905" TEXT="... if it&apos;s original"/>
+<node CREATED="1591425656245" ID="ID_1186866326" MODIFIED="1591425659841" TEXT="... it will be linked to!"/>
+</node>
+<node CREATED="1591425806299" ID="ID_887523164" MODIFIED="1591425812803" POSITION="left" TEXT="Do it with style">
+<icon BUILTIN="full-5"/>
+<node CREATED="1591425813462" ID="ID_167247105" MODIFIED="1591425817104" TEXT="Visually engage your reader"/>
+</node>
+<node CREATED="1591425867923" ID="ID_868239776" MODIFIED="1591425869781" POSITION="left" TEXT="Do it to scale">
+<icon BUILTIN="full-6"/>
+<node CREATED="1591425923187" ID="ID_895036642" MODIFIED="1591425927767" TEXT="Have a large collection of things"/>
+<node CREATED="1591425928129" ID="ID_794809524" MODIFIED="1591425934647" TEXT="... and you&apos;ll be the &quot;expert&quot;"/>
+<node CREATED="1591425935050" ID="ID_739322719" MODIFIED="1591425957754" TEXT="Example">
+<icon BUILTIN="wizard"/>
+<node CREATED="1591425936635" ID="ID_47703506" MODIFIED="1591425946407" TEXT="Large collection of Mountain pictures"/>
+<node CREATED="1591425947267" ID="ID_495973778" MODIFIED="1591425954983" TEXT="... you&apos;re the expert in mountain pictures!"/>
+</node>
+</node>
+<node CREATED="1591425986946" ID="ID_166538847" MODIFIED="1591425991334" POSITION="left" TEXT="Make your content easy to share">
+<icon BUILTIN="full-7"/>
+<node CREATED="1591426077426" ID="ID_218323028" MODIFIED="1591426084854" TEXT="It could be a widget or whatever"/>
+<node CREATED="1591426085570" ID="ID_355665079" MODIFIED="1591426099270" TEXT="Make everything easy for people linking to you"/>
+</node>
+<node CREATED="1591426192871" ID="ID_765303358" MODIFIED="1591426196194" POSITION="left" TEXT="Write like an Authority">
+<icon BUILTIN="full-8"/>
+<node CREATED="1591430751859" ID="ID_1398653251" MODIFIED="1591430761119" TEXT="When you achieve authority in one concept"/>
+<node CREATED="1591430761474" ID="ID_1256393225" MODIFIED="1591430766527" TEXT="... people will link more to you"/>
+<node CREATED="1591430767374" ID="ID_902027460" MODIFIED="1591430770647" TEXT="... which brings more authority"/>
+</node>
+<node CREATED="1591425660748" ID="ID_529506553" MODIFIED="1591425666824" POSITION="right" TEXT="Creating resources that other people like to use">
+<icon BUILTIN="full-4"/>
+<node CREATED="1591425701932" ID="ID_511423560" MODIFIED="1591425714296" TEXT="Expand your offering to folks which can link to you"/>
+<node CREATED="1591425715660" ID="ID_1160204077" MODIFIED="1591425727456" TEXT="Valuable when you&apos;re in a market"/>
+<node CREATED="1591425727892" ID="ID_1213197563" MODIFIED="1591425734174" TEXT="... where there is no blog / nothing which can link"/>
+<node CREATED="1591425776844" ID="ID_841120541" MODIFIED="1591425786353" TEXT="Example">
+<icon BUILTIN="wizard"/>
+<node CREATED="1591425778588" ID="ID_900403934" MODIFIED="1591425784064" TEXT="Writing about rail programming"/>
+</node>
+</node>
+</node>
+</map>
