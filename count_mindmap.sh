6ff8c1fc830a4658045e402414cc3e208f37992f
matthieu <matthieu.cneude@gmail.com>
2020-09-09 16:31:21 +0200

    Try to make a script

---
+++ count_mindmap.sh
@@ -0,0 +1,6 @@
+count=$(ls **/*.mm | wc -l)
+replace="\(https\:\/\/img\.shields\.io\/badge\/total_mindmap.*\)"
+c="https\:\/\/img\.shields\.io\/badge\/total_mindmap\-$count\-blue\)"
+sed -i "s/$replace/$c/" README.md
+
+git add README.md
