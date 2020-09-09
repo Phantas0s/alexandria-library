f8006735f29120861a9050ae3c4f4745ca5c579c
matthieu <matthieu.cneude@gmail.com>
2020-09-09 16:33:39 +0200

    Little fix on count script

--- count_mindmap.sh
+++ count_mindmap.sh
@@ -3,6 +3,6 @@
 count=$(ls **/*.mm | wc -l)
 replace="\(https\:\/\/img\.shields\.io\/badge\/total_mindmap.*\)"
 c="https\:\/\/img\.shields\.io\/badge\/total_mindmap\-$count\-blue\)"
-sed -i "s/$replace/$c/" README.md
+sed -i "s/$replace/$c" README.md
 
 git add README.md
