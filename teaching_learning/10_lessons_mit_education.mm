bf04522a28422c1ee3cc39b9cadd9cc753acd1bd
matthieu <matthieu.cneude@gmail.com>
2020-10-09 14:05:26 +0200

    10 lessons of MIT education

---
+++ teaching_learning/10_lessons_mit_education.mm
@@ -0,0 +1,97 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1602243450100" ID="ID_1764878151" LINK="https://www.math.tamu.edu/~cyan/Rota/mitless.html" MODIFIED="1602243458153" TEXT="10 Lessons of an MIT Education ">
+<node CREATED="1602243933969" ID="ID_277836547" MODIFIED="1602243943338" POSITION="right" TEXT="You can and will work at a desk for seven hours straight, routinely">
+<icon BUILTIN="full-1"/>
+<node CREATED="1602243957737" ID="ID_1172927404" MODIFIED="1602243966179" TEXT="Being a teacher means..."/>
+<node CREATED="1602243967289" ID="ID_996090743" MODIFIED="1602243995385" TEXT="Every sentence correctly enunciated"/>
+<node CREATED="1602243999513" ID="ID_1625744036" MODIFIED="1602244004324" TEXT="Examples must be relevant"/>
+<node CREATED="1602244006383" ID="ID_879283770" MODIFIED="1602244011309" TEXT="... even fascinating"/>
+<node CREATED="1602244015864" ID="ID_1846454414" MODIFIED="1602244018612" TEXT="Every 15 minutes">
+<node CREATED="1602244018824" ID="ID_1751483966" MODIFIED="1602244019973" TEXT="Aside"/>
+<node CREATED="1602244020439" ID="ID_1621701449" MODIFIED="1602244021340" TEXT="Joke"/>
+<node CREATED="1602244021704" ID="ID_1171365313" MODIFIED="1602244023043" TEXT="Good story"/>
+<node CREATED="1602244028616" ID="ID_695104783" MODIFIED="1602244036548" TEXT="Unusual application of concept"/>
+</node>
+<node CREATED="1602244059496" ID="ID_1084299479" MODIFIED="1602244073468" TEXT="Need discipline and intensive constant work"/>
+</node>
+<node CREATED="1602243467099" ID="ID_1406094268" MODIFIED="1602243932293" POSITION="right" TEXT="You learn what you don&apos;t know you are learning">
+<icon BUILTIN="full-2"/>
+<node CREATED="1602243578332" ID="ID_1518147063" MODIFIED="1602243586542" TEXT="Difficult exercises need to be tried"/>
+<node CREATED="1602243587541" ID="ID_1465574713" MODIFIED="1602243594342" TEXT="... even if the solution is given afterward"/>
+<node CREATED="1602243614988" ID="ID_89269581" MODIFIED="1602243640104" TEXT="Trying to understand the solution teach a lot"/>
+</node>
+<node CREATED="1602243652587" ID="ID_127185656" MODIFIED="1602243929903" POSITION="right" TEXT="By and large, &quot;knowing how&quot; matters more than &quot;knowing what&quot;">
+<icon BUILTIN="full-3"/>
+<node CREATED="1602243668259" ID="ID_312289212" MODIFIED="1602243672141" TEXT="&quot;knowing how&quot; courses">
+<node CREATED="1602243743317" ID="ID_438654135" MODIFIED="1602243759016" TEXT="Examples">
+<icon BUILTIN="wizard"/>
+<node CREATED="1602243672754" ID="ID_1679136198" MODIFIED="1602243674397" TEXT="Mathematics"/>
+<node CREATED="1602243674907" ID="ID_393489697" MODIFIED="1602243684079" TEXT="Exact sciences"/>
+<node CREATED="1602243684443" ID="ID_311447499" MODIFIED="1602243686023" TEXT="Engineering"/>
+<node CREATED="1602243686363" ID="ID_1262357356" MODIFIED="1602243692511" TEXT="Musical instrument"/>
+<node CREATED="1602243692851" ID="ID_579214057" MODIFIED="1602243693671" TEXT="Sport"/>
+</node>
+<node CREATED="1602243770105" ID="ID_1622354374" MODIFIED="1602243836589" TEXT="Held in higher esteem">
+<node CREATED="1602243847305" ID="ID_1065917123" MODIFIED="1602243849918" TEXT="Can be tested"/>
+<node CREATED="1602243864120" ID="ID_220754256" MODIFIED="1602243870598" TEXT="... high standard of proficiency"/>
+<node CREATED="1602243870937" ID="ID_1650701990" MODIFIED="1602243875949" TEXT="... everyone agrees"/>
+</node>
+</node>
+<node CREATED="1602243694523" ID="ID_1628807109" MODIFIED="1602243699037" TEXT="&quot;knowing what&quot; courses">
+<node CREATED="1602243736794" ID="ID_852870623" MODIFIED="1602243767995" TEXT="Examples">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="wizard"/>
+<node CREATED="1602243699442" ID="ID_1329311812" MODIFIED="1602243707607" TEXT="Social science"/>
+<node CREATED="1602243707971" ID="ID_214844514" MODIFIED="1602243713599" TEXT="Creative arts"/>
+<node CREATED="1602243715898" ID="ID_1305003445" MODIFIED="1602243718663" TEXT="Humanities"/>
+</node>
+<node CREATED="1602243781137" ID="ID_525763743" MODIFIED="1602243785630" TEXT="Content more memorable"/>
+</node>
+</node>
+<node CREATED="1602243923890" ID="ID_1863446423" MODIFIED="1602244085572" POSITION="right" TEXT="In science and engineering, you can fool very little of the time">
+<icon BUILTIN="full-4"/>
+<node CREATED="1602244113680" ID="ID_1007032834" MODIFIED="1602244119996" TEXT="Need to learn intellectual honesty">
+<node CREATED="1602244120152" ID="ID_1546779915" MODIFIED="1602244122268" TEXT="What you know"/>
+<node CREATED="1602244122591" ID="ID_552775923" MODIFIED="1602244125563" TEXT="What you don&apos;t know"/>
+<node CREATED="1602244213535" ID="ID_1045304342" MODIFIED="1602244219953" TEXT="Learn whether or not you&apos;ve learned"/>
+</node>
+<node CREATED="1602244126208" ID="ID_993090018" MODIFIED="1602244130844" TEXT="Can be seen as naivete"/>
+<node CREATED="1602244151706" ID="ID_34304493" MODIFIED="1602244156107" TEXT="... but real strength"/>
+</node>
+<node CREATED="1602244234830" ID="ID_1234702470" MODIFIED="1602244286138" POSITION="right" TEXT="You don&apos;t have to be a genius to do creative work">
+<icon BUILTIN="full-5"/>
+<node CREATED="1602244286654" ID="ID_500664495" MODIFIED="1602244290682" TEXT="Realise that even the best"/>
+<node CREATED="1602244291302" ID="ID_1144899845" MODIFIED="1602244293187" TEXT="... make mistakes"/>
+<node CREATED="1602244293550" ID="ID_1881598323" MODIFIED="1602244299194" TEXT="... and they made even more"/>
+<node CREATED="1602244299702" ID="ID_1387960203" MODIFIED="1602244302249" TEXT="... to be where they are"/>
+</node>
+<node CREATED="1602244309430" ID="ID_496916568" MODIFIED="1602244311499" POSITION="right" TEXT=" You must measure up to a very high level of performance">
+<icon BUILTIN="full-6"/>
+<node CREATED="1602244348302" ID="ID_1331713705" MODIFIED="1602244355593" TEXT="Doesn&apos;t matter much about experience of the teacher"/>
+<node CREATED="1602244355926" ID="ID_1125700096" MODIFIED="1602244525698" TEXT="... for "/>
+<node CREATED="1602244536333" ID="ID_246956798" MODIFIED="1602244540247" TEXT="What matters the most is ambiance"/>
+<node CREATED="1602244544253" ID="ID_1467738744" MODIFIED="1602244552544" TEXT="... in which course is taught"/>
+<node CREATED="1602244592451" ID="ID_231486237" MODIFIED="1602244598874" TEXT="Expectation of high standard"/>
+<node CREATED="1602244604212" ID="ID_262486334" MODIFIED="1602244611319" TEXT="... unconsciously adopted by student"/>
+</node>
+<node CREATED="1602244620876" ID="ID_88823113" MODIFIED="1602244623058" POSITION="right" TEXT="The world and your career are unpredictable, so you are better off learning subjects of permanent value">
+<icon BUILTIN="full-7"/>
+<node CREATED="1602244687483" ID="ID_207216423" MODIFIED="1602244691536" TEXT="MIT teach fundamentals"/>
+<node CREATED="1602244691883" ID="ID_715354529" MODIFIED="1602244695872" TEXT="... which are not likely to change"/>
+<node CREATED="1602244696179" ID="ID_1862632240" MODIFIED="1602244699856" TEXT="... with technological shifts"/>
+</node>
+<node CREATED="1602244737627" ID="ID_465625979" MODIFIED="1602244745126" POSITION="right" TEXT="You are never going to catch up, and neither is anyone else">
+<icon BUILTIN="full-8"/>
+<node CREATED="1602244810386" ID="ID_1554388180" MODIFIED="1602244815143" TEXT="Leisure disappear for everybody"/>
+</node>
+<node CREATED="1602244825172" ID="ID_604446120" MODIFIED="1602244826857" POSITION="right" TEXT=" The future belongs to the computer-literate-squared">
+<icon BUILTIN="full-9"/>
+</node>
+<node CREATED="1602244962824" ID="ID_1723596087" MODIFIED="1602244965665" POSITION="right" TEXT="Mathematics is still the queen of the sciences">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-0"/>
+<node CREATED="1602245019105" ID="ID_639099824" MODIFIED="1602245029764" TEXT="MIT is just a huge applied mathematics department"/>
+</node>
+</node>
+</map>
