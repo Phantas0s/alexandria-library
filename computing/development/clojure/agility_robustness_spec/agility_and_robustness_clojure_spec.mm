cb440ac70e4dc9b2d2b731c0a4f51525ccc4231b
matthieu <matthieu.cneude@gmail.com>
2020-11-25 18:06:12 +0100

    Map clojure spec presentation by Stuart Halloway

---
+++ computing/development/clojure/agility_robustness_spec/agility_and_robustness_clojure_spec.mm
@@ -0,0 +1,102 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1606322213498" ID="ID_623098678" LINK="https://www.youtube.com/watch?v=VNTQ-M_uSo8" MODIFIED="1606322290471" TEXT="Agility &amp; Robustness: Clojure spec">
+<node CREATED="1606322292733" ID="ID_1860468065" MODIFIED="1606323471180" POSITION="right" TEXT="Clojure power">
+<icon BUILTIN="full-1"/>
+<node CREATED="1606322400532" ID="ID_1621627276" MODIFIED="1606322402239" TEXT="Generality">
+<node CREATED="1606322403548" ID="ID_1930871782" MODIFIED="1606322429464" TEXT="All domains use same general purpose data structure"/>
+<node CREATED="1606322433298" ID="ID_1351296228" MODIFIED="1606322434968" TEXT="Systemic">
+<node CREATED="1606322435163" ID="ID_865815312" MODIFIED="1606322462588" TEXT="Examples">
+<icon BUILTIN="wizard"/>
+<node CREATED="1606322437212" ID="ID_238432572" MODIFIED="1606322438520" TEXT="Libs"/>
+<node CREATED="1606322439164" ID="ID_1866526912" MODIFIED="1606322440392" TEXT="Apps"/>
+<node CREATED="1606322441219" ID="ID_357651233" MODIFIED="1606322442952" TEXT="Config"/>
+<node CREATED="1606322443568" ID="ID_532451510" MODIFIED="1606322444984" TEXT="On wire"/>
+</node>
+</node>
+</node>
+<node CREATED="1606322498340" ID="ID_794065835" MODIFIED="1606322500320" TEXT="Clojure promise">
+<node CREATED="1606322500499" ID="ID_179313088" MODIFIED="1606322506158" TEXT="Talk of precise things"/>
+<node CREATED="1606322506718" ID="ID_722975697" MODIFIED="1606322508631" TEXT="... in general ways">
+<node CREATED="1606322515315" ID="ID_795630838" MODIFIED="1606322518104" TEXT="needs discipline"/>
+</node>
+<node CREATED="1606322530946" ID="ID_819554921" MODIFIED="1606322532959" TEXT="Scary?"/>
+<node CREATED="1606322533374" ID="ID_1014935919" MODIFIED="1606322537199" TEXT="Winning tradeoffs?"/>
+</node>
+<node CREATED="1606322476593" ID="ID_633446771" MODIFIED="1606322477702" TEXT="Problem">
+<node CREATED="1606322477866" ID="ID_1811017276" MODIFIED="1606322485702" TEXT="Systemic generalities are great"/>
+<node CREATED="1606322486307" ID="ID_1329988683" MODIFIED="1606322490952" TEXT="... but we work with specificity"/>
+</node>
+<node CREATED="1606323367094" ID="ID_1238212454" MODIFIED="1606323374127" TEXT="Illustration">
+<icon BUILTIN="licq"/>
+<node CREATED="1606323375085" ID="ID_1097265188" MODIFIED="1606323380961">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="clojure_power.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1606323409757" ID="ID_1355224095" MODIFIED="1606323472158" POSITION="right" TEXT="Spec power">
+<icon BUILTIN="full-2"/>
+<node CREATED="1606322567818" ID="ID_1752110327" MODIFIED="1606323490553" TEXT="Spec answer challenge">
+<node CREATED="1606322577610" ID="ID_1296586769" MODIFIED="1606322582879" TEXT="Deliver specificity a la carte"/>
+</node>
+<node CREATED="1606323416412" ID="ID_194529868" MODIFIED="1606323420529" TEXT="Illustration">
+<icon BUILTIN="licq"/>
+<node CREATED="1606323442228" ID="ID_654937702" MODIFIED="1606323459107">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="spec_power.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1606323505604" ID="ID_246333083" MODIFIED="1606323509920" TEXT="Correct / agile / robust">
+<node CREATED="1606323510156" ID="ID_138501003" MODIFIED="1606323566121" TEXT="Illustration">
+<icon BUILTIN="licq"/>
+<node CREATED="1606323557952" ID="ID_900612833" MODIFIED="1606323563091">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="robust.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1606323609060" ID="ID_1381992566" MODIFIED="1606323611776" TEXT="Spec leverage">
+<node CREATED="1606323613003" ID="ID_817145287" MODIFIED="1606323620090" TEXT="Illustration">
+<icon BUILTIN="licq"/>
+<node CREATED="1606323620660" ID="ID_1661432128" MODIFIED="1606323628187">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="spec_leverage.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1606323946722" ID="ID_368405443" MODIFIED="1606323948262" TEXT="When?">
+<node CREATED="1606323948673" ID="ID_593358363" MODIFIED="1606323951094" TEXT="Up to you"/>
+<node CREATED="1606323951802" ID="ID_143858099" MODIFIED="1606323954718" TEXT="Doc is automatic"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
