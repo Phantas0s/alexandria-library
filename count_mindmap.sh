bf7597714ff17fde28bbe97874043563fb8fa4f8
matthieu <matthieu.cneude@gmail.com>
2021-01-26 07:33:01 +0100

    Slight improvement on count mindmap

--- count_mindmap.sh
+++ count_mindmap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-count=$(ls **/*.mm | wc -l)
-replace="\(https\:\/\/img\.shields\.io\/badge\/total_mindmaps.*\)"
-c="https\:\/\/img\.shields\.io\/badge\/total_mindmaps\-$count\-blue\)"
-sed -i "s/$replace/$c" README.md
+count=$(find -- **/*.mm | wc -l)
+pattern="\(https\:\/\/img\.shields\.io\/badge\/total_mindmaps.*\)"
+replace="https\:\/\/img\.shields\.io\/badge\/total_mindmaps\-$count\-blue\)"
+sed -i "s/$pattern/$replace" README.md
 
 git add README.md
