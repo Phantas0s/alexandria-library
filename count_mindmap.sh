af7284f80a6d1c08859546d8c6b550770b8ae7b3
matthieu <matthieu.cneude@gmail.com>
2021-01-26 08:05:01 +0100

    Imrpove count_mindmap

--- count_mindmap.sh
+++ count_mindmap.sh
@@ -4,5 +4,3 @@ count=$(find -- **/*.mm | wc -l)
 pattern="https://img\.shields\.io/badge/total_mindmaps.*"
 replace="https://img\.shields\.io/badge/total_mindmaps\-$count\-blue)"
 sed -i "s#$pattern#$replace#" README.md
-
-git add README.md
