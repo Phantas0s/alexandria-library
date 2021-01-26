067e7bb3df4ce83703470f95b70811ba77a07eb8
matthieu <matthieu.cneude@gmail.com>
2021-01-26 14:33:00 +0100

    Improve script count mindmap

--- count_mindmap.sh
+++ count_mindmap.sh
@@ -2,5 +2,5 @@
 
 count=$(find -- **/*.mm | wc -l)
 pattern="https://img\.shields\.io/badge/total_mindmaps.*"
-replace="https://img\.shields\.io/badge/total_mindmaps\-$count\-blue)"
+replace="https://img.shields.io/badge/total_mindmaps-$count-blue)"
 sed -i "s#$pattern#$replace#" README.md
