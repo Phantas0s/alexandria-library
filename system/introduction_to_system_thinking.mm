64fdb05de349e2168ee6654303505f5bd8cf8a32
matthieu <matthieu.cneude@gmail.com>
2021-03-06 18:06:56 +0100

    Begin introduction to general system thinking

---
+++ system/introduction_to_system_thinking.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1615050071886" ID="ID_963020349" MODIFIED="1615050274907" TEXT="An Introduction to General System Thinking"/>
+</map>
