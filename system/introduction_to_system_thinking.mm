c0d33089e416489988f46db7b9420560c8b76c23
matthieu <matthieu.cneude@gmail.com>
2021-05-17 16:09:07 +0200

    Continue introduction to system thinking book

--- system/introduction_to_system_thinking.mm
+++ system/introduction_to_system_thinking.mm
@@ -1,4 +1,255 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1615050071886" ID="ID_963020349" MODIFIED="1615050274907" TEXT="An Introduction to General System Thinking"/>
+<node CREATED="1615050071886" ID="ID_963020349" MODIFIED="1615050274907" TEXT="An Introduction to General System Thinking">
+<node CREATED="1621257742869" ID="ID_151587739" MODIFIED="1621257793120" POSITION="right" TEXT="Preface to silver anniversary edition">
+<node CREATED="1621257796964" ID="ID_316980391" MODIFIED="1621257809832" TEXT="Albert Einstein">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The significant problems we face cannot be solved at the same level of thinking we were at when we
+    </p>
+    <p>
+      created them. - Albert Einstein
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1621257838487" ID="ID_126339439" MODIFIED="1621257843081" POSITION="right" TEXT="Original Preface">
+<node CREATED="1621257850492" ID="ID_1644739741" MODIFIED="1621257855240" TEXT="People have foggy ideas">
+<node CREATED="1621257868421" ID="ID_1267478312" MODIFIED="1621257872553" TEXT="... about what they are saying"/>
+<node CREATED="1621257872988" ID="ID_1123970982" MODIFIED="1621257884697" TEXT="... how translating thoughts in computer programs"/>
+</node>
+<node CREATED="1621257888356" ID="ID_447327596" MODIFIED="1621257894072" TEXT="Need fog clearing techniques">
+<node CREATED="1621257897172" ID="ID_115031636" MODIFIED="1621257900689" TEXT="Way of thinking"/>
+<node CREATED="1621257901003" ID="ID_240806725" MODIFIED="1621257903593" TEXT="... a programmer could have"/>
+</node>
+</node>
+<node CREATED="1621257913796" ID="ID_1348859439" MODIFIED="1621257921317" POSITION="right" TEXT="How To Use ThisBook"/>
+<node CREATED="1621257932420" ID="ID_154349581" MODIFIED="1621257946435" POSITION="right" TEXT="Chapter 1 - The problem">
+<icon BUILTIN="full-1"/>
+<node CREATED="1621257984147" FOLDED="true" ID="ID_738939494" MODIFIED="1621257988386" TEXT="The complexity of the world">
+<icon BUILTIN="full-1"/>
+<node CREATED="1621257992795" ID="ID_926026419" MODIFIED="1621257998119" TEXT="First step of knowledge"/>
+<node CREATED="1621258002163" ID="ID_1968157046" MODIFIED="1621258006752" TEXT="... is confession of ignorance"/>
+<node CREATED="1621258012259" ID="ID_1756690912" MODIFIED="1621258020224" TEXT="Need to acknowledge our ignorance"/>
+<node CREATED="1621258023181" ID="ID_1870583397" MODIFIED="1621258030766" TEXT="General systems is child of science"/>
+</node>
+<node CREATED="1621258033083" FOLDED="true" ID="ID_304259399" MODIFIED="1621258045216" TEXT="Mechanism and mechanics">
+<icon BUILTIN="full-2"/>
+<node CREATED="1621258052363" ID="ID_1473882486" MODIFIED="1621258066150" TEXT="Examine Physics for general view of science"/>
+<node CREATED="1621258068867" ID="ID_1802522110" MODIFIED="1621258070126" TEXT="Physic">
+<node CREATED="1621258070340" ID="ID_680523899" MODIFIED="1621258085039" TEXT="Study of objects in precise environment"/>
+<node CREATED="1621258087563" ID="ID_104283558" MODIFIED="1621258090496" TEXT="&quot;Law of nature&quot;"/>
+</node>
+<node CREATED="1621258096771" ID="ID_1752297143" MODIFIED="1621258098784" TEXT="Mechanics">
+<node CREATED="1621258139459" ID="ID_1160166086" MODIFIED="1621258143923" TEXT="Karl Deutsch">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#8230; implied the notion of a whole which was completely equal to the sum of its parts; which could
+    </p>
+    <p>
+      be run in reverse; and which would behave in exactly identical fashion no matter how often these
+    </p>
+    <p>
+      parts were disassembled and put together again, and irrespective of the sequence in which the
+    </p>
+    <p>
+      disassembling or reassembling would take place. It implied consequently that the parts were never
+    </p>
+    <p>
+      significantly modified by each other, nor by their own past, and that each part once placed in its
+    </p>
+    <p>
+      appropriate position with its appropriate momentum, would stay exactly there and continue to fulfill
+    </p>
+    <p>
+      its completely and uniquely determined function.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1621258217323" ID="ID_406735859" MODIFIED="1621258242152" TEXT="Why mechanics considered base of science?">
+<node CREATED="1621258242659" ID="ID_1403942644" MODIFIED="1621258245383" TEXT="Very limited"/>
+<node CREATED="1621258251523" ID="ID_1678848436" MODIFIED="1621258267311" TEXT="Only aproximatisation of mechanical systems"/>
+</node>
+<node CREATED="1621258271826" ID="ID_1242849706" MODIFIED="1621258275094" TEXT="Complex mechanical systems"/>
+<node CREATED="1621258275419" ID="ID_1964678363" MODIFIED="1621258279487" TEXT="... can be reduced to simpler ones">
+<node CREATED="1621258282626" ID="ID_988029833" MODIFIED="1621258285935" TEXT="Only then formal methods"/>
+<node CREATED="1621258286274" ID="ID_1974554226" MODIFIED="1621258291662" TEXT="... can do their work"/>
+</node>
+<node CREATED="1621258297914" ID="ID_11376904" MODIFIED="1621258400299" TEXT="Example">
+<icon BUILTIN="wizard"/>
+<node CREATED="1621258299169" ID="ID_106228404" MODIFIED="1621258300958" TEXT="Solar systems"/>
+<node CREATED="1621258301251" ID="ID_1888033974" MODIFIED="1621258309407" TEXT="Special tractable case of bodies in motion"/>
+<node CREATED="1621258366058" ID="ID_1461974996" MODIFIED="1621258369205" TEXT="But we know it&apos;s wrong">
+<node CREATED="1621258372265" ID="ID_1948977720" MODIFIED="1621258374101" TEXT="Many more bodies"/>
+</node>
+<node CREATED="1621258382089" ID="ID_410935546" MODIFIED="1621258388071" TEXT="... ignored to focus on motion for example"/>
+<node CREATED="1621258390586" ID="ID_1865462955" MODIFIED="1621258398981" TEXT="... influence of other bodies &quot;too small&quot;"/>
+</node>
+</node>
+<node CREATED="1621258403602" FOLDED="true" ID="ID_1009646857" MODIFIED="1621258415000" TEXT="The square law of computation">
+<icon BUILTIN="full-3"/>
+<node CREATED="1621258449634" ID="ID_672538761" MODIFIED="1621258465509" TEXT="Need to understand">
+<node CREATED="1621258419490" ID="ID_1503589461" MODIFIED="1621258462421" TEXT="How each object behave in isolation">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1621258439001" ID="ID_227565035" MODIFIED="1621258447286" TEXT="How each body affect the other">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1621258467353" ID="ID_67088595" MODIFIED="1621258477702" TEXT="How things behave if there is no body">
+<icon BUILTIN="full-3"/>
+<node CREATED="1621258513017" ID="ID_43407957" MODIFIED="1621258521742" TEXT="The &quot;field equation&quot;"/>
+</node>
+</node>
+<node CREATED="1621258489801" ID="ID_418302267" MODIFIED="1621258495006" TEXT="Most general 2 bodies system">
+<node CREATED="1621258495242" ID="ID_96699570" MODIFIED="1621258500278" TEXT="2 isolated equations"/>
+<node CREATED="1621258502873" ID="ID_1884247549" MODIFIED="1621258507404" TEXT="1 interaction equation"/>
+<node CREATED="1621258508212" ID="ID_275242263" MODIFIED="1621258511885" TEXT="1 field equation"/>
+</node>
+<node CREATED="1621258531424" ID="ID_487911414" MODIFIED="1621258533805" TEXT="More there is body">
+<node CREATED="1621258534721" ID="ID_1841440779" MODIFIED="1621258543948" TEXT="more interaction equation rise"/>
+<node CREATED="1621258548697" ID="ID_545381589" MODIFIED="1621258554428" TEXT="n bodies">
+<node CREATED="1621258555473" ID="ID_1626953323" MODIFIED="1621258560558" TEXT="2^n relationships"/>
+</node>
+</node>
+<node CREATED="1621258566696" ID="ID_809116051" MODIFIED="1621258574404" TEXT="Need to lower the number of equations">
+<node CREATED="1621258576553" ID="ID_1137259696" MODIFIED="1621258583053" TEXT="Example">
+<icon BUILTIN="wizard"/>
+<node CREATED="1621258577889" ID="ID_1439114743" MODIFIED="1621258581308" TEXT="We don&apos;t care about small bodies"/>
+</node>
+</node>
+<node CREATED="1621258588153" ID="ID_244143326" MODIFIED="1621258693874" TEXT="Square Law of Computation">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1621258594465" ID="ID_139048409" MODIFIED="1621258599212" TEXT="Amount of computation increase"/>
+<node CREATED="1621258599689" ID="ID_796663812" MODIFIED="1621258607661" TEXT="... as the size of the problem increase"/>
+<node CREATED="1621258695585" ID="ID_23296236" MODIFIED="1621258712773" TEXT="...at least as the square of number or equations"/>
+<node CREATED="1621258713545" ID="ID_1022303068" MODIFIED="1621258719221" TEXT="... if no simplification is made"/>
+</node>
+<node CREATED="1621258741312" ID="ID_331903881" MODIFIED="1621258755575" TEXT="Example">
+<icon BUILTIN="wizard"/>
+<node CREATED="1621258743592" ID="ID_561202046" MODIFIED="1621258745972" TEXT="If double equations"/>
+<node CREATED="1621258746287" ID="ID_666354318" MODIFIED="1621258753771" TEXT="... need computer 4 times as fast"/>
+<node CREATED="1621258762432" ID="ID_797231121" MODIFIED="1621258769797" TEXT="... to solve things in good amount of time"/>
+</node>
+<node CREATED="1621258798351" ID="ID_686358573" MODIFIED="1621258810276" TEXT="Limit of size of a system of equation can be solved"/>
+</node>
+<node CREATED="1621258818352" FOLDED="true" ID="ID_1119911307" MODIFIED="1621258822527" TEXT="The Simplification of Science and the Science of Simplification">
+<icon BUILTIN="full-4"/>
+<node CREATED="1621258840455" ID="ID_957803564" MODIFIED="1621258844693" TEXT="Practical computation">
+<node CREATED="1621258844920" ID="ID_266146045" MODIFIED="1621258855228" TEXT="Implicit assumptions need to be brought"/>
+<node CREATED="1621258855527" ID="ID_1100623524" MODIFIED="1621258859763" TEXT="... out into the open">
+<node CREATED="1621258861800" ID="ID_4436374" MODIFIED="1621258865331" TEXT="Programmer study"/>
+<node CREATED="1621258865655" ID="ID_1906333089" MODIFIED="1621258870683" TEXT="... how people make assumptions"/>
+</node>
+</node>
+<node CREATED="1621258877520" ID="ID_744812732" MODIFIED="1621258880668" TEXT="Mechanics">
+<node CREATED="1621258880952" ID="ID_1595998777" MODIFIED="1621258890659" TEXT="Need to assume that some interactions"/>
+<node CREATED="1621258890999" ID="ID_1986152758" MODIFIED="1621258894389" TEXT="... are more important than others"/>
+<node CREATED="1621258896377" ID="ID_427995030" MODIFIED="1621258903344" TEXT="Example">
+<icon BUILTIN="wizard"/>
+<node CREATED="1621258897480" ID="ID_1793635181" MODIFIED="1621258901140" TEXT="Gravity in solar system"/>
+</node>
+<node CREATED="1621258922543" ID="ID_949053294" MODIFIED="1621258923573" TEXT="Why?">
+<node CREATED="1621258923839" ID="ID_749414881" MODIFIED="1621258933108" TEXT="They give satisfactory answers"/>
+<node CREATED="1621258933479" ID="ID_146302512" MODIFIED="1621258942875" TEXT="... means match observational data"/>
+</node>
+</node>
+<node CREATED="1621258945295" ID="ID_1590029001" MODIFIED="1621258955131" TEXT="Greatest generalization achieved">
+<node CREATED="1621258956063" ID="ID_130847060" MODIFIED="1621258957827" TEXT="Newton"/>
+<node CREATED="1621258958214" ID="ID_1978854755" MODIFIED="1621258965067" TEXT="F=GMm/r^2">
+<node CREATED="1621258968151" ID="ID_150961598" MODIFIED="1621258971618" TEXT="M">
+<node CREATED="1621258971806" ID="ID_1036716925" MODIFIED="1621258975154" TEXT="Mass of first object"/>
+</node>
+<node CREATED="1621258975919" ID="ID_1941364114" MODIFIED="1621258977571" TEXT="m">
+<node CREATED="1621258977863" ID="ID_1015414402" MODIFIED="1621258980588" TEXT="Mass of second object"/>
+</node>
+<node CREATED="1621258982096" ID="ID_1317508254" MODIFIED="1621258983690" TEXT="r">
+<node CREATED="1621258983838" ID="ID_1165304187" MODIFIED="1621258987587" TEXT="Distance between object"/>
+</node>
+<node CREATED="1621258988534" ID="ID_1608209381" MODIFIED="1621258992131" TEXT="G">
+<node CREATED="1621258992423" ID="ID_997650143" MODIFIED="1621258996554" TEXT="Universal constant"/>
+</node>
+</node>
+<node CREATED="1621259018863" ID="ID_1052673865" MODIFIED="1621259033091" TEXT="State implicitly that no more equation needed"/>
+<node CREATED="1621259040119" ID="ID_989765012" MODIFIED="1621259104980" TEXT="... but only about for pair">
+<node CREATED="1621259089622" ID="ID_785012786" MODIFIED="1621259094658" TEXT="simplification"/>
+</node>
+<node CREATED="1621259112527" ID="ID_179811692" MODIFIED="1621259118395" TEXT="Works in mechanics but not in psychology"/>
+<node CREATED="1621259140566" ID="ID_1019792479" MODIFIED="1621259555915" TEXT="For solar system">
+<node CREATED="1621259143177" ID="ID_1460215878" MODIFIED="1621259155595" TEXT="reduce equations from 1000 to 45">
+<node CREATED="1621259160926" ID="ID_1743301630" MODIFIED="1621259165419" TEXT="10 things taken in pair"/>
+</node>
+<node CREATED="1621259179367" ID="ID_729158773" MODIFIED="1621259183506" TEXT="Square law of computation">
+<node CREATED="1621259192750" ID="ID_606543424" MODIFIED="1621259200187" TEXT="Size of our task decrease"/>
+<node CREATED="1621259200551" ID="ID_646637364" MODIFIED="1621259212443" TEXT="... by square of 1000/45"/>
+<node CREATED="1621259439991" ID="ID_1593659462" MODIFIED="1621259449155" TEXT="... or about 100 time"/>
+</node>
+<node CREATED="1621259522990" ID="ID_1584185497" MODIFIED="1621259528818" TEXT="Consider mass of Sun bigger"/>
+<node CREATED="1621259534414" ID="ID_256307563" MODIFIED="1621259538739" TEXT="... only consider pair with the sun"/>
+</node>
+</node>
+<node CREATED="1621259564965" ID="ID_1278453175" MODIFIED="1621259573195" TEXT="Separating systems into non-interactive systems">
+<node CREATED="1621259575901" ID="ID_371101547" MODIFIED="1621259580242" TEXT="Extremely important in science"/>
+<node CREATED="1621259585334" ID="ID_190822947" MODIFIED="1621259592395" TEXT="n^2 computation if there is interaction"/>
+<node CREATED="1621259592694" ID="ID_1959405" MODIFIED="1621259594939" TEXT="... n otherwise"/>
+<node CREATED="1621259635438" ID="ID_843126063" MODIFIED="1621259648281" TEXT="Square Law of computation put a limit of any computing device"/>
+<node CREATED="1621259649030" ID="ID_706850523" MODIFIED="1621259655522" TEXT="The human brain is some sort of computing device"/>
+<node CREATED="1621259660238" ID="ID_455776697" MODIFIED="1621259670458" TEXT="If we want to survive in a complex world, we need all the help"/>
+<node CREATED="1621259672195" ID="ID_654682217" MODIFIED="1621259676443" TEXT="Newton was a genius">
+<node CREATED="1621259676630" ID="ID_394067805" MODIFIED="1621259680699" TEXT="Not because of his brain"/>
+<node CREATED="1621259681134" ID="ID_1363136398" MODIFIED="1621259716890" TEXT="... but because of his ability to take world and">
+<node CREATED="1621259696794" ID="ID_1132634731" MODIFIED="1621259699322" TEXT="simplify"/>
+<node CREATED="1621259699671" ID="ID_1151537353" MODIFIED="1621259706898" TEXT="idealize"/>
+<node CREATED="1621259707191" ID="ID_1446317358" MODIFIED="1621259712329" TEXT="streamline"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1621259730453" FOLDED="true" ID="ID_1882848459" MODIFIED="1621259745795" TEXT="Statistical Mechanics and the Law of Large Number">
+<icon BUILTIN="full-5"/>
+<node CREATED="1621259755269" ID="ID_1365580898" MODIFIED="1621259761946" TEXT="Newton achievement"/>
+<node CREATED="1621259762988" ID="ID_1953092307" MODIFIED="1621259770202" TEXT="Describing system with around 105 objects"/>
+<node CREATED="1621259772318" ID="ID_1682351723" MODIFIED="1621259777666" TEXT="... of which found 10 of interest"/>
+<node CREATED="1621259782173" ID="ID_1831716278" MODIFIED="1621259785506" TEXT="Law of Large Number">
+<node CREATED="1621259791550" ID="ID_107207473" MODIFIED="1621259798426" TEXT="Large the population"/>
+<node CREATED="1621259798765" ID="ID_1576178466" MODIFIED="1621259802418" TEXT="... more likely we are"/>
+<node CREATED="1621259802782" ID="ID_794959466" MODIFIED="1621259806136" TEXT=".. to observe values"/>
+<node CREATED="1621259806454" ID="ID_261172566" MODIFIED="1621259810401" TEXT="... close to predicted"/>
+<node CREATED="1621259810703" ID="ID_55572007" MODIFIED="1621259813794" TEXT="... average value"/>
+</node>
+<node CREATED="1621259830998" ID="ID_1430991832" MODIFIED="1621259853554" TEXT="Statistical mechanics">
+<node CREATED="1621259854902" ID="ID_1687921441" MODIFIED="1621259861450" TEXT="&quot;Unorganized complexity&quot;"/>
+<node CREATED="1621259867626" ID="ID_1355817229" MODIFIED="1621259875434" TEXT="Complex systems"/>
+<node CREATED="1621259882117" ID="ID_1016962811" MODIFIED="1621259884810" TEXT="Sufficiently random"/>
+<node CREATED="1621259885438" ID="ID_1714437690" MODIFIED="1621259894114" TEXT="... so sufficiently regular"/>
+<node CREATED="1621259895779" ID="ID_1210334651" MODIFIED="1621259902338" TEXT="... to be studied statistically"/>
+</node>
+</node>
+<node COLOR="#338800" CREATED="1621260440268" ID="ID_187186169" MODIFIED="1621260456428" TEXT="It&apos;s not possible to summarize the following - see book">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="password"/>
+<node COLOR="#338800" CREATED="1621260462891" ID="ID_274035021" MODIFIED="1621260468213" TEXT="Important passage">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1621260489747" ID="ID_759025249" MODIFIED="1621260498192" TEXT="The Law of Medium Numbers">
+<icon BUILTIN="full-6"/>
+</node>
+</node>
+<node CREATED="1621260511219" ID="ID_851146633" MODIFIED="1621260530312" POSITION="right" TEXT="Chapter 2 - The Approach">
+<icon BUILTIN="full-2"/>
+</node>
+</node>
 </map>
