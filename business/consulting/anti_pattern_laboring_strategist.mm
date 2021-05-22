87ff28f443e5d123c1aac36abbcf56d5db2eb1d8
matthieu <matthieu.cneude@gmail.com>
2021-05-22 08:13:53 +0200

    Add antipattern labor strategist

---
+++ business/consulting/anti_pattern_laboring_strategist.mm
@@ -0,0 +1,221 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1621659949984" ID="ID_1738165226" LINK="https://daedtech.com/the-laboring-strategist-a-free-agent-anti-pattern-and-how-to-fix/" MODIFIED="1621660471401" TEXT="The Laboring Strategist, A Free-Agent Anti-Pattern (And How to Fix) ">
+<node CREATED="1621659972189" ID="ID_14716830" MODIFIED="1621660035360" POSITION="right" TEXT="Introduction">
+<icon BUILTIN="full-0"/>
+</node>
+<node CREATED="1621660023016" ID="ID_539132043" MODIFIED="1621660041749" POSITION="right" TEXT="Introducing The Solo Content Marketer ">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1621660053504" ID="ID_1016559462" MODIFIED="1621660058046" POSITION="right" TEXT="What Does Solo Content Marketing Involve?">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1621660095863" ID="ID_1555162268" MODIFIED="1621660098896" POSITION="right" TEXT="Content Marketer: A Laboring-Strategist">
+<icon BUILTIN="full-3"/>
+<node CREATED="1621660112447" ID="ID_1591862604" MODIFIED="1621660120572" TEXT="4 phases of commercial problem">
+<node CREATED="1621660121148" ID="ID_757039023" MODIFIED="1621660126244" TEXT="Diagnosis of a problem">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1621660121149" ID="ID_187295537" MODIFIED="1621660128005" TEXT="Prescription of a therapy">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1621660121151" ID="ID_724730805" MODIFIED="1621660131144" TEXT="Application of the therapy">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1621660121152" ID="ID_1826933264" MODIFIED="1621660133303" TEXT="Re-application and maintenance of the therapy">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+<node CREATED="1621660241014" ID="ID_1669961386" MODIFIED="1621660249675" TEXT="Does a certification give you more credibility?"/>
+</node>
+<node CREATED="1621660216855" ID="ID_483231465" MODIFIED="1621660230829" POSITION="right" TEXT="The Plight of the Laboring-Strategist">
+<icon BUILTIN="full-4"/>
+<node CREATED="1621660237094" ID="ID_441898003" MODIFIED="1621660237593" TEXT="Jim and the Content Marketer ">
+<node CREATED="1621660264783" ID="ID_991383190" MODIFIED="1621660270787" TEXT="When you do labor and consultancy"/>
+<node CREATED="1621660272119" ID="ID_600597784" MODIFIED="1621660277203" TEXT="... most people asked you for labor"/>
+<node CREATED="1621660277575" ID="ID_1994522300" MODIFIED="1621660280363" TEXT="... not consultancy!"/>
+</node>
+<node CREATED="1621660298990" ID="ID_83818078" MODIFIED="1621660299829" TEXT="Sandy and the Software Consultant">
+<node CREATED="1621660393662" ID="ID_327517633" MODIFIED="1621660399666" TEXT="Developer doing consultancy"/>
+<node CREATED="1621660399982" ID="ID_1113465495" MODIFIED="1621660418810" TEXT="... is a developer giving unsolicited opinions about everything"/>
+</node>
+<node CREATED="1621660423622" ID="ID_1546706024" MODIFIED="1621660424094" TEXT="You and Your Personal Trainer "/>
+</node>
+<node CREATED="1621660448070" ID="ID_1673862520" MODIFIED="1621660453518" POSITION="right" TEXT="Overcoming the Laboring-Strategist Slog">
+<icon BUILTIN="full-5"/>
+<node CREATED="1621660462521" ID="ID_1754990840" MODIFIED="1621660462521" TEXT="Once a Laborer, Always a Laborer">
+<node CREATED="1621660532182" ID="ID_688252710" MODIFIED="1621660535554" TEXT="Even if you go up the ladder"/>
+<node CREATED="1621660536254" ID="ID_826361426" MODIFIED="1621660541042" TEXT="... in more strategic positions"/>
+<node CREATED="1621660541342" ID="ID_128477671" MODIFIED="1621660546850" TEXT="... people will always see you as laborer"/>
+</node>
+<node CREATED="1621660529205" ID="ID_955255516" MODIFIED="1621660613794" TEXT="Once a Strategist, Always a Strategist">
+<node CREATED="1621660605846" ID="ID_607295210" MODIFIED="1621660612794" TEXT="Way of engaging people"/>
+<node CREATED="1621660625494" ID="ID_73010411" MODIFIED="1621660633586" TEXT="Give strategy, not labor">
+<node CREATED="1621660633782" ID="ID_1642615468" MODIFIED="1621660636738" TEXT="Not negociable"/>
+</node>
+<node CREATED="1621660645510" ID="ID_1403335038" MODIFIED="1621660649354" TEXT="Need to be engaged in">
+<node CREATED="1621660649614" ID="ID_1498566739" MODIFIED="1621660654778" TEXT="diagnosis"/>
+<node CREATED="1621660655101" ID="ID_757688203" MODIFIED="1621660658013" TEXT="prescription"/>
+</node>
+</node>
+<node CREATED="1621660666590" ID="ID_1511634381" MODIFIED="1621660675730" TEXT="Avoid laboring strategy anti-pattern">
+<node CREATED="1621660676646" FOLDED="true" ID="ID_1872558146" MODIFIED="1621660852438" TEXT="Take Inventory of Your Current Positioning">
+<icon BUILTIN="full-1"/>
+<node CREATED="1621660707062" ID="ID_535701432" MODIFIED="1621660774327" TEXT="Can you articulate how your engagement will">
+<icon BUILTIN="full-1"/>
+<node CREATED="1621660709958" ID="ID_1852143674" MODIFIED="1621660725378" TEXT="Make or save client money?"/>
+<node CREATED="1621660714998" ID="ID_432426193" MODIFIED="1621660722978" TEXT="How they&apos;ll earn a return on investment?"/>
+</node>
+<node CREATED="1621660727941" ID="ID_594370731" MODIFIED="1621660778514" TEXT="Are people paying for your advice?">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1621660749318" ID="ID_1988946014" MODIFIED="1621660779073" TEXT="Is your deliverable a blueprint for someone to execute?">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1621660760166" ID="ID_1621988383" MODIFIED="1621660780003" TEXT="Do you define the process by which clients engage you, or do they?">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1621660803901" ID="ID_1016759095" MODIFIED="1621660807834" TEXT="... if no, you&apos;re in labor"/>
+</node>
+<node CREATED="1621660812758" FOLDED="true" ID="ID_1492824327" MODIFIED="1621660814382" TEXT=" Declare Bankruptcy on the Laboring-Strategist Model">
+<icon BUILTIN="full-2"/>
+<node CREATED="1621660827446" ID="ID_263220430" MODIFIED="1621660829546" TEXT="Need to retool">
+<node CREATED="1621660829773" ID="ID_1462788868" MODIFIED="1621660831002" TEXT="approach"/>
+<node CREATED="1621660831297" ID="ID_1259511878" MODIFIED="1621660832960" TEXT="positioning"/>
+</node>
+</node>
+<node CREATED="1621660855110" FOLDED="true" ID="ID_1477986572" MODIFIED="1621660867035" TEXT="Bifurcate Your Offerings into Strategy and Labor">
+<icon BUILTIN="full-3"/>
+<node CREATED="1621660875886" ID="ID_1435175" MODIFIED="1621660894170" TEXT="List of activities you&apos;ve performed for clients (or would perform)">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1621660887734" ID="ID_53920211" MODIFIED="1621660894865" TEXT="Put them into two buckets">
+<icon BUILTIN="full-2"/>
+<node CREATED="1621660895710" ID="ID_1220860050" MODIFIED="1621660898910" TEXT="Strategy">
+<icon BUILTIN="full-1"/>
+<node CREATED="1621660121148" ID="ID_1852233219" MODIFIED="1621660912307" TEXT="Diagnosis of a problem"/>
+<node CREATED="1621660121149" ID="ID_536792801" MODIFIED="1621660913528" TEXT="Prescription of a therapy"/>
+</node>
+<node CREATED="1621660905270" ID="ID_1908523046" MODIFIED="1621660916500" TEXT="Labor">
+<icon BUILTIN="full-2"/>
+<node CREATED="1621660121151" ID="ID_336638797" MODIFIED="1621660914503" TEXT="Application of the therapy"/>
+<node CREATED="1621660121152" ID="ID_1717733883" MODIFIED="1621660915501" TEXT="Re-application and maintenance of the therapy"/>
+</node>
+</node>
+<node CREATED="1621660931486" ID="ID_427109699" MODIFIED="1621660946025" TEXT="Create first class offering from 1st bucket">
+<icon BUILTIN="full-3"/>
+<node CREATED="1621660949790" ID="ID_376024022" MODIFIED="1621660959009" TEXT="Look at market that offering to future prospects"/>
+<node CREATED="1621660963662" ID="ID_940628232" MODIFIED="1621660970265" TEXT="These prospects will see you as strategist"/>
+</node>
+<node CREATED="1621661368011" ID="ID_999424108" MODIFIED="1621661383113" TEXT="Keep your current clients for paying the bill">
+<icon BUILTIN="full-4"/>
+<node CREATED="1621661374202" ID="ID_361799806" MODIFIED="1621661380542" TEXT="Search new client on strategic part"/>
+</node>
+<node CREATED="1621661298517" ID="ID_189814278" MODIFIED="1621661395542" TEXT="Example">
+<icon BUILTIN="wizard"/>
+<node CREATED="1621661396129" ID="ID_1511310819" MODIFIED="1621661400859" TEXT="Some strategic engagement"/>
+<node CREATED="1621661301621" ID="ID_1073820827" MODIFIED="1621661408461" TEXT="Discovery process"/>
+<node CREATED="1621661336656" ID="ID_1513012888" MODIFIED="1621661343823" TEXT="Current state assessments"/>
+<node CREATED="1621661344193" ID="ID_1048273308" MODIFIED="1621661352679" TEXT="Retainer advice engagement"/>
+</node>
+</node>
+<node CREATED="1621661416504" FOLDED="true" ID="ID_837166741" MODIFIED="1621661427824" TEXT="Assess the possibility of owning all 4 phases">
+<icon BUILTIN="full-4"/>
+<node CREATED="1621661451923" ID="ID_924137297" MODIFIED="1621661457236" TEXT="For software developers">
+<node CREATED="1621661457471" ID="ID_1740288809" MODIFIED="1621661466004" TEXT="Organizations without in-house development"/>
+<node CREATED="1621661468335" ID="ID_1281855296" MODIFIED="1621661473107" TEXT="Need some work done"/>
+</node>
+<node CREATED="1621661473783" ID="ID_404581909" MODIFIED="1621661502280" TEXT="Example">
+<icon BUILTIN="wizard"/>
+<node CREATED="1621661475055" ID="ID_503877828" MODIFIED="1621661483395" TEXT="Website revamps for non-profit"/>
+<node CREATED="1621661483975" ID="ID_696647181" MODIFIED="1621661493474" TEXT="Specialization in customization of piece of software"/>
+<node CREATED="1621661493863" ID="ID_246687100" MODIFIED="1621661500835" TEXT="... used by specific manufacturing industries"/>
+</node>
+<node CREATED="1621661509134" ID="ID_242889549" MODIFIED="1621661524282" TEXT="Answer strategic questions and take care of implementation"/>
+<node CREATED="1621661557941" ID="ID_348171536" MODIFIED="1621661562925" TEXT="More planning in B2B">
+<node CREATED="1621661562926" ID="ID_1901064245" MODIFIED="1621661564977" TEXT="Bigger project"/>
+<node CREATED="1621661565260" ID="ID_1432336283" MODIFIED="1621661568144" TEXT="Experts in house"/>
+</node>
+</node>
+<node CREATED="1621661574972" FOLDED="true" ID="ID_715945973" MODIFIED="1621661580280" TEXT="Cultivate a Referral Network for Labor">
+<icon BUILTIN="full-5"/>
+<node CREATED="1621661600348" ID="ID_423512846" MODIFIED="1621661607040" TEXT="Don&apos;t need to be out of the labor business altogether"/>
+<node CREATED="1621661634435" ID="ID_1681193861" MODIFIED="1621661638127" TEXT="Steps can be">
+<node CREATED="1621661638306" ID="ID_459194822" MODIFIED="1621661642469" TEXT="Discovery engagement">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1621661644282" ID="ID_1745351799" MODIFIED="1621661650315" TEXT="Start executing resultant roadmap">
+<icon BUILTIN="full-2"/>
+<node CREATED="1621661654882" ID="ID_1394316007" MODIFIED="1621661657599" TEXT="Might refer out"/>
+<node CREATED="1621661657994" ID="ID_823055896" MODIFIED="1621661662694" TEXT="Handle with subcontractors"/>
+<node CREATED="1621661663178" ID="ID_208925948" MODIFIED="1621661665462" TEXT="Handle myself"/>
+<node CREATED="1621661666794" ID="ID_1918654987" MODIFIED="1621661672326" TEXT="... depending how discovery goes"/>
+</node>
+</node>
+<node CREATED="1621661705489" ID="ID_1283850312" MODIFIED="1621661713421" TEXT="Labor is not the focus, just implementation details"/>
+</node>
+<node CREATED="1621661714977" FOLDED="true" ID="ID_793028362" MODIFIED="1621661723665" TEXT="Graudllay Thin Out Your Laboring Engagements">
+<icon BUILTIN="full-6"/>
+<node CREATED="1621661728993" ID="ID_1811733485" MODIFIED="1621661737565" TEXT="Could go cold turkey but bills to pay"/>
+<node CREATED="1621661754912" ID="ID_1533419618" MODIFIED="1621661766253" TEXT="Try to slowly backfill labor with strategy instead"/>
+<node CREATED="1621661786752" ID="ID_289783968" MODIFIED="1621661793380" TEXT="Tell client you want to end labor engagements"/>
+<node CREATED="1621661809832" ID="ID_1691253555" MODIFIED="1621661813196" TEXT="Labor relationships">
+<node CREATED="1621661813736" ID="ID_1630447233" MODIFIED="1621661816748" TEXT="Easy to sell"/>
+<node CREATED="1621661819328" ID="ID_1275260548" MODIFIED="1621661824043" TEXT="Often open-ended"/>
+<node CREATED="1621661824399" ID="ID_569107484" MODIFIED="1621661826412" TEXT="Durable"/>
+<node CREATED="1621661826768" ID="ID_409656109" MODIFIED="1621661828300" TEXT="Long lasting"/>
+</node>
+<node CREATED="1621661836631" ID="ID_1932692097" MODIFIED="1621661842115" TEXT="Strategy relationships">
+<node CREATED="1621661842335" ID="ID_1081840535" MODIFIED="1621661844540" TEXT="More expensive"/>
+<node CREATED="1621661844903" ID="ID_1680279277" MODIFIED="1621661848731" TEXT="Ephemeral in nature">
+<node CREATED="1621661851512" ID="ID_1703476120" MODIFIED="1621661858763" TEXT="Long term strategy"/>
+<node CREATED="1621661859143" ID="ID_981741622" MODIFIED="1621661865675" TEXT="... handled by company&apos;s executives"/>
+</node>
+<node CREATED="1621661869391" ID="ID_1079219635" MODIFIED="1621661876305" TEXT="Flip from few clients per year"/>
+<node CREATED="1621661876655" ID="ID_638307501" MODIFIED="1621661880467" TEXT="... to few clients per month"/>
+</node>
+<node CREATED="1621661883927" ID="ID_1925088568" MODIFIED="1621661885883" TEXT="Lay out a plan">
+<node CREATED="1621661886087" ID="ID_1581151462" MODIFIED="1621661891235" TEXT="Price strategy engagements flat"/>
+<node CREATED="1621661892460" ID="ID_1126800338" MODIFIED="1621661895666" TEXT="Productized service"/>
+<node CREATED="1621661896055" ID="ID_938043497" MODIFIED="1621661919258" TEXT="Timebox engeagements">
+<node CREATED="1621661919446" ID="ID_89627370" MODIFIED="1621661921314" TEXT="Plan their fit"/>
+<node CREATED="1621661921638" ID="ID_1585923299" MODIFIED="1621661925003" TEXT="... into your work calendar"/>
+</node>
+<node CREATED="1621661929622" ID="ID_161110863" MODIFIED="1621661942186" TEXT="Help identify critical mass at which you need to sunset labor client"/>
+</node>
+</node>
+<node CREATED="1621661944222" FOLDED="true" ID="ID_200417645" MODIFIED="1621661968145" TEXT="Market your strategy offerings like there&apos;s not tomorrow">
+<icon BUILTIN="full-7"/>
+<node CREATED="1621661978054" ID="ID_1184243197" MODIFIED="1621661984098" TEXT="Market your strategy offering like crazy">
+<node CREATED="1621661984245" ID="ID_1591358453" MODIFIED="1621661988882" TEXT="NOT SELL"/>
+</node>
+<node CREATED="1621661997093" ID="ID_395910537" MODIFIED="1621662010105" TEXT="Spend long time to speak about ">
+<node CREATED="1621662010389" ID="ID_398532035" MODIFIED="1621662013961" TEXT="strategy offering"/>
+<node CREATED="1621662014349" ID="ID_270043569" MODIFIED="1621662016505" TEXT="related concerns"/>
+</node>
+<node CREATED="1621662028533" ID="ID_1638643779" MODIFIED="1621662034193" TEXT="What are your 4 steps discovery process?">
+<node CREATED="1621662035917" ID="ID_1139216972" MODIFIED="1621662042329" TEXT="Talk in depth about each of them"/>
+<node CREATED="1621662042829" ID="ID_1632800867" MODIFIED="1621662047426" TEXT="Write blog posts about them"/>
+<node CREATED="1621662047760" ID="ID_1340115576" MODIFIED="1621662052249" TEXT="Create videos about them"/>
+<node CREATED="1621662053250" ID="ID_1055359999" MODIFIED="1621662057193" TEXT="Make case studies about them"/>
+<node CREATED="1621662059825" ID="ID_1560256444" MODIFIED="1621662065505" TEXT="Promote the content and do AMA"/>
+</node>
+<node CREATED="1621662070853" ID="ID_275481163" MODIFIED="1621662077505" TEXT="... do it in a way to genuinely help people"/>
+<node CREATED="1621662081301" ID="ID_1877721422" MODIFIED="1621662089736" TEXT="... who have struggled with discovery"/>
+<node CREATED="1621662099955" ID="ID_946411858" MODIFIED="1621662107281" TEXT="Answer common client questions about discovery">
+<node CREATED="1621662107948" ID="ID_1159095741" MODIFIED="1621662110472" TEXT="With your content"/>
+<node CREATED="1621662110753" ID="ID_81240841" MODIFIED="1621662113696" TEXT="With your actual offering"/>
+</node>
+<node CREATED="1621662128284" ID="ID_267927493" MODIFIED="1621662140768" TEXT="Answer every question out there around strategy offering"/>
+</node>
+</node>
+</node>
+<node CREATED="1621662166524" ID="ID_1306828212" MODIFIED="1621662183292" POSITION="right" TEXT="No Shade to Laboring-Strategists">
+<icon BUILTIN="full-6"/>
+<node CREATED="1621662198932" ID="ID_1286962227" MODIFIED="1621662206919" TEXT="Laboring strategist is a frustrating slog"/>
+<node CREATED="1621662224507" ID="ID_1082571434" MODIFIED="1621662231410" TEXT="They have the right knowledge and advice"/>
+<node CREATED="1621662231755" ID="ID_1217896495" MODIFIED="1621662240575" TEXT="They have the wrong, disinterested audience"/>
+<node CREATED="1621662246043" ID="ID_1554730637" MODIFIED="1621662257239" TEXT="More successful business is often finding the right audience"/>
+</node>
+</node>
+</map>
