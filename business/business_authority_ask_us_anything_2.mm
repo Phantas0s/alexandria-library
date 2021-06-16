d45fb8d3e0ab4f22fa9cad71db50f6d6788a9876
matthieu <matthieu.cneude@gmail.com>
2021-06-16 14:04:15 +0200

    Add second business of authority "ask us anything 2"

---
+++ business/business_authority_ask_us_anything_2.mm
@@ -0,0 +1,115 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1623751585862" ID="ID_156124014" LINK="https://www.thebusinessofauthority.com/episodes/ask-us-anything-2" MODIFIED="1623752879363" TEXT="Ask us anything 2">
+<node CREATED="1623751615756" ID="ID_1446240558" MODIFIED="1623751704685" POSITION="right" TEXT="Digital content and lead">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p http-equiv="content-type" content="text/html; charset=utf-8" style="font-weight: 400; font-style: normal; font-variant: normal; font-size: 14px; line-height: 24px; font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, Helvetica, Arial, sans-serif; margin-top: 0px; margin-bottom: 10px; color: rgb(15, 15, 15); letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px">
+      How to create memorable digital content and leads that build a pipeline of future clients/customers.
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1623751704877" ID="ID_1327914792" MODIFIED="1623751709001" TEXT="Need to find a niche"/>
+<node CREATED="1623752008397" ID="ID_1624177381" MODIFIED="1623752024290" TEXT="Automated marketing system"/>
+</node>
+<node CREATED="1623752110693" ID="ID_1637772797" MODIFIED="1623752129487" POSITION="right" TEXT="Social capital">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p http-equiv="content-type" content="text/html; charset=utf-8" style="font-weight: 400; font-style: normal; font-variant: normal; font-size: 14px; line-height: 24px; font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, Helvetica, Arial, sans-serif; margin-top: 0px; margin-bottom: 10px; color: rgb(15, 15, 15); letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px">
+      What role does social capital play on the road to authority?
+    </p>
+    <br class="Apple-interchange-newline" />
+    
+  </body>
+</html>
+</richcontent>
+<node CREATED="1623752136717" ID="ID_1781752072" MODIFIED="1623752494646" TEXT="Trust is important"/>
+<node CREATED="1623752494995" ID="ID_1780641563" MODIFIED="1623752497088" TEXT="Focus is important"/>
+</node>
+<node CREATED="1623752499651" ID="ID_415938819" MODIFIED="1623752851124" POSITION="right" TEXT="Value price engagement">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(15, 15, 15)" face="-apple-system, BlinkMacSystemFont, Helvetica Neue, Helvetica, Arial, sans-serif">Positioning yourself to value price engagements when your projects don&#8217;t have an end date&#8212;and you&#8217;re currently charging hourly or by retainer.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1623752518763" ID="ID_1919741707" MODIFIED="1623752526656" TEXT="Value pricing for project with particular outcome">
+<node CREATED="1623752570284" ID="ID_249502326" MODIFIED="1623752575516" TEXT="Need deadline"/>
+<node CREATED="1623752555315" ID="ID_1511271246" MODIFIED="1623752557968" TEXT="If there is not value"/>
+<node CREATED="1623752558292" ID="ID_1997159642" MODIFIED="1623752562982" TEXT="... doesn&apos;t make sense to have conversation"/>
+</node>
+<node CREATED="1623752578683" ID="ID_1189162447" MODIFIED="1623752582633" TEXT="Subscription model">
+<node CREATED="1623752582787" ID="ID_753121617" MODIFIED="1623752589088" TEXT="Pay monthly for access to expertise">
+<node CREATED="1623752624003" ID="ID_1317795464" MODIFIED="1623752649735" TEXT="Less change to have some hourly calculation"/>
+</node>
+<node CREATED="1623752593518" ID="ID_33581947" MODIFIED="1623752596227" TEXT="If it&apos;s hourly">
+<node CREATED="1623752596228" ID="ID_1478222382" MODIFIED="1623752610447" TEXT="might be some hand made"/>
+</node>
+</node>
+<node CREATED="1623752808210" ID="ID_348908712" MODIFIED="1623752809938" TEXT="Go up">
+<node CREATED="1623752809939" ID="ID_1016881491" MODIFIED="1623752817191" TEXT="Don&apos;t do the handwork for small marketing agency"/>
+<node CREATED="1623752817506" ID="ID_236218675" MODIFIED="1623752819095" TEXT="Go up">
+<node CREATED="1623752819242" ID="ID_1208357594" MODIFIED="1623752820689" TEXT="Middle size"/>
+<node CREATED="1623752820995" ID="ID_128733416" MODIFIED="1623752825350" TEXT="Bring some strategy"/>
+</node>
+</node>
+</node>
+<node CREATED="1623752851978" ID="ID_33104162" MODIFIED="1623752865743" POSITION="right" TEXT="Positioning music and musician">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(15, 15, 15)" face="-apple-system, BlinkMacSystemFont, Helvetica Neue, Helvetica, Arial, sans-serif">How to apply positioning in the context of music and musicians&#8212;is this process different when dealing with an &#8220;esoteric&#8221; specialty?</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1623752914114" ID="ID_1246459072" MODIFIED="1623752916374" TEXT="Music for ">
+<node CREATED="1623752916867" ID="ID_499468337" MODIFIED="1623752918321" TEXT="business?"/>
+<node CREATED="1623752918666" ID="ID_1474654736" MODIFIED="1623752920238" TEXT="art?"/>
+</node>
+<node CREATED="1623752920987" ID="ID_144465050" MODIFIED="1623752933070" TEXT="... very different and need to make that decision"/>
+<node CREATED="1623752961906" ID="ID_1690506720" MODIFIED="1623752965238" TEXT="If business">
+<node CREATED="1623752984202" ID="ID_432007308" MODIFIED="1623753000722" TEXT="Value pricing">
+<node CREATED="1623753009249" ID="ID_792329764" MODIFIED="1623753017749" TEXT="Desire * time * buying power / availability of options"/>
+</node>
+<node CREATED="1623752965474" ID="ID_386962905" MODIFIED="1623752969013" TEXT="What&apos;s value for music?"/>
+<node CREATED="1623752969290" ID="ID_763568683" MODIFIED="1623752973558" TEXT="What&apos;s value for your music?"/>
+<node CREATED="1623752973850" ID="ID_1169212599" MODIFIED="1623752977662" TEXT="Where to get the highest value?"/>
+</node>
+<node CREATED="1623753123321" ID="ID_1848815371" MODIFIED="1623753128038" TEXT="It&apos;s really hard and brutal"/>
+</node>
+<node CREATED="1623753270792" ID="ID_782407175" MODIFIED="1623753289605" POSITION="right" TEXT="What&apos;s next for the business of authority">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p http-equiv="content-type" content="text/html; charset=utf-8" style="font-weight: 400; font-style: normal; font-variant: normal; font-size: 14px; line-height: 24px; font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, Helvetica, Arial, sans-serif; margin-top: 0px; margin-bottom: 10px; color: rgb(15, 15, 15); letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px">
+      What do you see ahead for The Business of Authority as you hit 250 episodes in 2022?
+    </p>
+    <br class="Apple-interchange-newline" />
+    
+
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
