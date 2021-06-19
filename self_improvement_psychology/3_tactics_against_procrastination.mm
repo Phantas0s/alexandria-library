cee6a2aaedaa6056c807b36c979c3848a75ef532
matthieu <matthieu.cneude@gmail.com>
2021-06-19 12:06:21 +0200

    Add procrastination mindmap

---
+++ self_improvement_psychology/3_tactics_against_procrastination.mm
@@ -0,0 +1,69 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1624090492061" ID="ID_205017135" LINK="https://www.thebusinessofauthority.com/episodes/3-tactics-to-beat-procrastination" MODIFIED="1624090504062" TEXT="3 Tactics To Beat Procrastination">
+<node CREATED="1624090511705" ID="ID_478080665" MODIFIED="1624092494440" POSITION="right" TEXT="When you don&apos;t want to do something for weeks">
+<icon BUILTIN="full-1"/>
+<node CREATED="1624090656304" ID="ID_1406857791" MODIFIED="1624090663313" TEXT="If it&apos;s on the list for one month"/>
+<node CREATED="1624090660409" ID="ID_745382280" MODIFIED="1624090664768" TEXT="... you won&apos;t do it"/>
+<node CREATED="1624090543513" ID="ID_125182355" MODIFIED="1624090546645" TEXT="Be aware of the consequences"/>
+<node CREATED="1624090546962" ID="ID_702899185" MODIFIED="1624090573396" TEXT="Sometimes consequences are less"/>
+<node CREATED="1624090573785" ID="ID_1683790133" MODIFIED="1624090575973" TEXT="... than doing it"/>
+</node>
+<node CREATED="1624090578689" ID="ID_1276504347" MODIFIED="1624092495011" POSITION="right" TEXT="Coupling with something fun">
+<icon BUILTIN="full-2"/>
+<node CREATED="1624090592384" ID="ID_554034383" MODIFIED="1624090597548" TEXT="Example">
+<icon BUILTIN="wizard"/>
+<node CREATED="1624090584968" ID="ID_1911724719" MODIFIED="1624090594691" TEXT="Combining a good lunch"/>
+<node CREATED="1624090588264" ID="ID_1764050924" MODIFIED="1624090592061" TEXT="... with something boring to do"/>
+</node>
+</node>
+<node CREATED="1624090628232" ID="ID_1590885906" MODIFIED="1624092495551" POSITION="right" TEXT="If too much on todo list">
+<icon BUILTIN="full-3"/>
+<node CREATED="1624090632089" ID="ID_1935226404" MODIFIED="1624090635285" TEXT="You don&apos;t respect the list"/>
+<node CREATED="1624090638498" ID="ID_365696044" MODIFIED="1624090645532" TEXT="Stuck the garbage and forget about it"/>
+<node CREATED="1624090647161" ID="ID_1927015434" MODIFIED="1624090649612" TEXT="Pretty bad"/>
+<node CREATED="1624090802784" ID="ID_653792102" MODIFIED="1624090807853" TEXT="You&apos;ll feel bad each time you see it"/>
+<node CREATED="1624090854520" ID="ID_456056604" MODIFIED="1624090862668" TEXT="Could be a big sticky note"/>
+</node>
+<node CREATED="1624090866695" ID="ID_12876401" MODIFIED="1624092496199" POSITION="right" TEXT="Project instead of todo">
+<icon BUILTIN="full-4"/>
+<node CREATED="1624090870568" ID="ID_634882924" MODIFIED="1624090874604" TEXT="When you don&apos;t do a todo "/>
+<node CREATED="1624090875296" ID="ID_5823467" MODIFIED="1624090877348" TEXT="... might be too big"/>
+<node CREATED="1624090885360" ID="ID_1478282900" MODIFIED="1624090891124" TEXT="Put it outside"/>
+<node CREATED="1624090891418" ID="ID_17779716" MODIFIED="1624090892996" TEXT="Todo list">
+<node CREATED="1624090893159" ID="ID_1783260967" MODIFIED="1624090897210" TEXT="Smallest next action you can take"/>
+</node>
+</node>
+<node CREATED="1624091464934" ID="ID_338615668" MODIFIED="1624092496898" POSITION="right" TEXT="Fire client procrastination">
+<icon BUILTIN="full-5"/>
+<node CREATED="1624091474078" ID="ID_977252968" MODIFIED="1624091476043" TEXT="Fire the client">
+<node CREATED="1624091745326" ID="ID_1070522457" MODIFIED="1624091747890" TEXT="It&apos;s always possible"/>
+<node CREATED="1624091748293" ID="ID_1448201357" MODIFIED="1624091757362" TEXT="... even if you think you&apos;re the only one knowing the code"/>
+</node>
+<node CREATED="1624091476375" ID="ID_238381731" MODIFIED="1624091479259" TEXT="Do something nice">
+<node CREATED="1624091479454" ID="ID_923526452" MODIFIED="1624091481402" TEXT="2 weeks more"/>
+<node CREATED="1624091481686" ID="ID_1173491740" MODIFIED="1624091484634" TEXT="training on what you&apos;re doing"/>
+<node CREATED="1624091484991" ID="ID_1615674352" MODIFIED="1624091487474" TEXT="... on internal people"/>
+<node CREATED="1624091488055" ID="ID_1463614746" MODIFIED="1624091490730" TEXT="Replacement">
+<node CREATED="1624091490880" ID="ID_823659324" MODIFIED="1624091491770" TEXT="internal"/>
+<node CREATED="1624091492087" ID="ID_901087765" MODIFIED="1624091493834" TEXT="external"/>
+</node>
+</node>
+<node CREATED="1624091496846" ID="ID_1238826084" MODIFIED="1624091499890" TEXT="... give some courtesy"/>
+<node CREATED="1624091573287" ID="ID_1895985243" MODIFIED="1624091579699" TEXT="How stop a current relationship"/>
+<node CREATED="1624091580199" ID="ID_1502080453" MODIFIED="1624091588194" TEXT="... is a sign how you&apos;ll begin a new one"/>
+</node>
+<node CREATED="1624092104476" ID="ID_1690393152" MODIFIED="1624092497865" POSITION="right" TEXT="Expert stuff / new stuff">
+<icon BUILTIN="full-6"/>
+<node CREATED="1624092110189" ID="ID_276704940" MODIFIED="1624092120320" TEXT="Tendency pour all time to the task you&apos;re great at"/>
+</node>
+<node CREATED="1624092556043" ID="ID_1191850366" MODIFIED="1624092579803" POSITION="right" TEXT="Stop fooling yourself">
+<icon BUILTIN="full-7"/>
+<node CREATED="1624092560043" ID="ID_431792740" MODIFIED="1624092574064" TEXT="Can be scary to not trust yourself"/>
+<node CREATED="1624092661707" ID="ID_1517670634" MODIFIED="1624092667743" TEXT="Useful to define your &quot;danger zone&quot;">
+<node CREATED="1624092762994" ID="ID_1078761861" MODIFIED="1624092791101" TEXT="When you is aware of that"/>
+<node CREATED="1624092791457" ID="ID_66047296" MODIFIED="1624092797909" TEXT="... you can bulk everything together"/>
+</node>
+</node>
+</node>
+</map>
