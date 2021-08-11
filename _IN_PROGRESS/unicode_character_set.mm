98ca68903f4048d8726159b24ce4e0f4cef1ec95
Matthieu <matthieu.cneude@gmail.com>
2021-08-11 19:54:18 +0200

    Read and map cli design best practices

--- _IN_PROGRESS/unicode_character_set.mm
+++
@@ -1,4 +0,0 @@
-<map version="1.0.1">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1566625947976" ID="ID_646591823" LINK="https://www.joelonsoftware.com/2003/10/08/the-absolute-minimum-every-software-developer-absolutely-positively-must-know-about-unicode-and-character-sets-no-excuses/" MODIFIED="1566625964314" TEXT="Unicode - Character Set"/>
-</map>
