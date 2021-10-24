fe20ff3847f2cced30795895de082ec9b4f1d6f6
matthieu <matthieu.cneude@gmail.com>
2021-10-24 18:28:14 +0200

    Review self-improvement and "psychology"

---
+++ self_improvement_psychology/emotional_intelligence/emotional_intelligence.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1634552550931" ID="ID_1451933415" MODIFIED="1634552558597" TEXT="Emotional Intelligence"/>
+</map>
