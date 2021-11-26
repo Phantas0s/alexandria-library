d95e4f6dace273d955343e8ad5e2cf9af0f4c186
matthieu <matthieu.cneude@gmail.com>
2021-11-26 10:07:43 +0100

    Begin to map neovim lua guide

---
+++ computing/vim/nvim_lua_guide.mm
@@ -0,0 +1,42 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1637848150135" ID="ID_216598949" LINK="https://github.com/nanotee/nvim-lua-guide" MODIFIED="1637848170445" TEXT="Getting started using Lua in Neovim">
+<node CREATED="1637848172794" ID="ID_616207844" MODIFIED="1637848423088" POSITION="right" TEXT="Introduction">
+<icon BUILTIN="full-0"/>
+<node CREATED="1637848482175" ID="ID_1872621527" MODIFIED="1637848605819" TEXT="Sources for learning Lua">
+<node CREATED="1637848483391" ID="ID_934916014" LINK="https://github.com/Phantas0s/Notes" MODIFIED="1637848503986" TEXT="Lua quick guide"/>
+<node CREATED="1637848541512" ID="ID_348975732" LINK="https://learnxinyminutes.com/docs/lua/" MODIFIED="1637848547905" TEXT="Learn lua in Y minutes"/>
+<node CREATED="1637848561852" ID="ID_148049797" LINK="https://www.luascript.dev/learn" MODIFIED="1637848566224" TEXT="Luascript tutorial"/>
+<node CREATED="1637848573581" ID="ID_1870555696" LINK="http://lua-users.org/wiki/LuaDirectory" MODIFIED="1637848585634" TEXT="Lua users wiki"/>
+<node CREATED="1637848594287" ID="ID_1830507419" LINK="https://www.lua.org/manual/5.1/" MODIFIED="1637848602274" TEXT="Official reference manual"/>
+</node>
+<node CREATED="1637848627383" ID="ID_74871909" MODIFIED="1637848634298" TEXT="Existing tutorials for Lua in Neovim"/>
+<node CREATED="1637848643518" ID="ID_1588947348" MODIFIED="1637848646121" TEXT="Companion plugins"/>
+</node>
+<node CREATED="1637848650343" ID="ID_405417917" MODIFIED="1637848655136" POSITION="right" TEXT=" Where to put Lua files">
+<icon BUILTIN="full-1"/>
+<node CREATED="1637848685918" ID="ID_1481177509" MODIFIED="1637848688895" TEXT="init.lua">
+<icon BUILTIN="full-1"/>
+<node CREATED="1637848656215" ID="ID_1363148249" MODIFIED="1637848670730" TEXT="init.lua can be load"/>
+<node CREATED="1637848671023" ID="ID_339973357" MODIFIED="1637848673890" TEXT="Purelly optional"/>
+<node CREATED="1637848678077" ID="ID_1616824195" MODIFIED="1637848681873" TEXT=":help config">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1637848697277" ID="ID_890403269" MODIFIED="1637848749019" TEXT="Modules">
+<icon BUILTIN="full-2"/>
+<node CREATED="1637848703317" ID="ID_1902517611" MODIFIED="1637848718137" TEXT="Inside a lua folder in runtimepath"/>
+<node CREATED="1637848723454" ID="ID_267802116" MODIFIED="1637848727241" TEXT="*nix systems">
+<node CREATED="1637848727526" ID="ID_1588101365" MODIFIED="1637848746048" TEXT="~/.config/nvim/lya">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1637848733206" ID="ID_4488049" MODIFIED="1637848736083" TEXT="Windows">
+<node CREATED="1637848736293" ID="ID_445790716" MODIFIED="1637848745640" TEXT="~/AppData/Local/nvim/lua">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
