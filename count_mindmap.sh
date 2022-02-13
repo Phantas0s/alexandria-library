77d6893829a18f03602bcd598f2f5fc5293e8672
matthieu <matthieu.cneude@gmail.com>
2022-02-13 14:16:42 +0100

    Add list of mindmap in README

--- count_mindmap.sh
+++ count_mindmap.sh
@@ -4,14 +4,8 @@ count=$(find -- **/*.mm | wc -l)
 pattern="https://img\.shields\.io/badge/total_mindmaps.*"
 replace="https://img.shields.io/badge/total_mindmaps-$count-blue)"
 sed -i "s#$pattern#$replace#" README.md
-mindmaps=$(tree -P "*.mm")
 
-cd ../library/
-sed -i "s#$pattern#$replace#" README.md
 sed -Ei '/```bash/,$d' README.md
 echo '```bash' >> README.md
-echo $mindmaps >> README.md
+echo $(tree -P "*.mm") >> README.md
 echo '```' >> README.md
-git add README.md
-git commit -m "Update Alexandria Library"
-git push origin master
