16aff7617af9a729f94d32968029feb20b847622
Matthieu <matthieu.cneude@gmail.com>
2022-03-08 08:54:15 +0100

    Map a podcast

---
+++ business/you_dont_have_imposter_syndrome.mm
@@ -0,0 +1,82 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1646487437805" ID="ID_435895822" LINK="https://rochellemoulton.com/tboa_podcast/why-you-dont-have-imposter-syndrome/" MODIFIED="1646487674941" TEXT="You don&apos;t have imposter syndrome">
+<node CREATED="1646487472814" FOLDED="true" ID="ID_1734402985" MODIFIED="1646487475114" POSITION="right" TEXT="Seth Godin">
+<node CREATED="1646487455902" ID="ID_480538420" MODIFIED="1646487464506" TEXT="If you&apos;re not feeling like an imposter"/>
+<node CREATED="1646487467141" ID="ID_1402142503" MODIFIED="1646487471681" TEXT=" ... you&apos;re not working hard enough"/>
+</node>
+<node CREATED="1646487485430" FOLDED="true" ID="ID_467730607" MODIFIED="1646487501626" POSITION="right" TEXT=" If you work on something new">
+<node CREATED="1646487501829" ID="ID_733091668" MODIFIED="1646487530186" TEXT="That&apos;s normal you don&apos;t know if it&apos;s gonna work"/>
+<node CREATED="1646487530611" ID="ID_1638059274" MODIFIED="1646487539457" TEXT="Don&apos;t feel like an imposter"/>
+<node CREATED="1646487539838" ID="ID_501271231" MODIFIED="1646487544114" TEXT="... you&apos;re just a beginner"/>
+<node CREATED="1646487551220" ID="ID_1264505798" MODIFIED="1646487554689" TEXT="Classic resistance"/>
+</node>
+<node CREATED="1646487561310" FOLDED="true" ID="ID_660498745" MODIFIED="1646487564849" POSITION="right" TEXT=" Attempt of changing something">
+<node CREATED="1646487569741" ID="ID_780282095" MODIFIED="1646487577256" TEXT="Making life better for others"/>
+<node CREATED="1646487578309" ID="ID_528256234" MODIFIED="1646487645176" TEXT="Reframing imposter syndrome as resistance"/>
+<node CREATED="1646487637349" ID="ID_511647400" MODIFIED="1646487640105" TEXT="... is potentially useful"/>
+</node>
+<node CREATED="1646487648084" FOLDED="true" ID="ID_872305790" MODIFIED="1646487652031" POSITION="right" TEXT=" Tempting to self diagnose">
+<node CREATED="1646487652259" ID="ID_1332336568" MODIFIED="1646487660769" TEXT="&quot;I have imposter syndrome so I don&apos;t want to do that&quot;"/>
+<node CREATED="1646487661140" ID="ID_30229496" MODIFIED="1646487667528" TEXT="Is it more about fear? Why?"/>
+<node CREATED="1646487688037" ID="ID_299080910" MODIFIED="1646487700065" TEXT="Shouldn&apos;t be an excuse to put yourself out"/>
+<node CREATED="1646487702460" ID="ID_554635533" MODIFIED="1646487706040" TEXT="Like writer block">
+<node CREATED="1646487706244" ID="ID_1511654606" MODIFIED="1646487712344" TEXT="It&apos;s not you don&apos;t know what to write"/>
+<node CREATED="1646487712653" ID="ID_1523383041" MODIFIED="1646487720865" TEXT="... it&apos;s you want to write good stuff"/>
+</node>
+</node>
+<node CREATED="1646487907579" FOLDED="true" ID="ID_1668525202" MODIFIED="1646487910879" POSITION="right" TEXT=" The War of Art">
+<node CREATED="1646488148196" ID="ID_807943446" MODIFIED="1646488155182" TEXT="Book from Pressfield"/>
+<node CREATED="1646487911092" ID="ID_1963098940" MODIFIED="1646487922578" TEXT="Put the feeling of resistance on an enemy"/>
+<node CREATED="1646487924626" ID="ID_573398204" MODIFIED="1646487936961" TEXT="Help recognize when you slip and procrastinate"/>
+</node>
+<node CREATED="1646488001906" FOLDED="true" ID="ID_1164521328" MODIFIED="1646488006904" POSITION="right" TEXT=" How much I study, less I know">
+<node CREATED="1646488016805" ID="ID_1096702739" MODIFIED="1646488023984" TEXT="Start to figure out how do I niche down?"/>
+<node CREATED="1646488024853" ID="ID_1859228019" MODIFIED="1646488028942" TEXT="Where do I go from there?"/>
+<node CREATED="1646488029309" ID="ID_986381137" MODIFIED="1646488033640" TEXT="How do I think about this?"/>
+<node CREATED="1646488033973" ID="ID_1532585824" MODIFIED="1646488042313" TEXT="All of these are signals"/>
+<node CREATED="1646488043068" ID="ID_1493339760" MODIFIED="1646488048777" TEXT="... and we need to use them"/>
+<node CREATED="1646488060580" ID="ID_776349856" MODIFIED="1646488073073" TEXT="Realization that the surface area of your domain is way bigger"/>
+<node CREATED="1646488073428" ID="ID_879596472" MODIFIED="1646488081999" TEXT="... and your certainty goes away"/>
+</node>
+<node CREATED="1646488338564" FOLDED="true" ID="ID_270669055" MODIFIED="1646488343215" POSITION="right" TEXT=" Experimentation is fine">
+<node CREATED="1646488343413" ID="ID_891044990" MODIFIED="1646488355297" TEXT="Scientists are not imposters because they do experiments"/>
+<node CREATED="1646488355900" ID="ID_1281529798" MODIFIED="1646488367936" TEXT="Label yourself like a scientist"/>
+</node>
+<node CREATED="1646488441237" FOLDED="true" ID="ID_1886353777" MODIFIED="1646488446153" POSITION="right" TEXT=" Imposter syndrome">
+<node CREATED="1646488446356" ID="ID_8263724" MODIFIED="1646488450609" TEXT="Emphasis on me">
+<node CREATED="1646488450836" ID="ID_509106697" MODIFIED="1646488453224" TEXT="not on people"/>
+<node CREATED="1646488453523" ID="ID_1351535011" MODIFIED="1646488457390" TEXT=" not on audience"/>
+</node>
+<node CREATED="1646488458004" ID="ID_917846022" MODIFIED="1646488467527" TEXT=" Doesn&apos;t really work"/>
+<node CREATED="1646488469860" ID="ID_501628795" MODIFIED="1646488477800" TEXT="Think about how to help the audience">
+<node CREATED="1646488478316" ID="ID_325613303" MODIFIED="1646488485624" TEXT=" What would they want to hear and which would help them"/>
+<node CREATED="1646488486484" ID="ID_347949628" MODIFIED="1646488492721" TEXT="... and they don&apos;t already know"/>
+</node>
+<node CREATED="1646488630692" ID="ID_1459888468" MODIFIED="1646488634752" TEXT="Being better than someone else">
+<node CREATED="1646488634932" ID="ID_426981713" MODIFIED="1646488637640" TEXT="That&apos;s not the point"/>
+<node CREATED="1646488638091" ID="ID_1873165581" MODIFIED="1646488641496" TEXT="You&apos;re here to help"/>
+<node CREATED="1646488641884" ID="ID_340594796" MODIFIED="1646488644959" TEXT="It&apos;s not about you"/>
+</node>
+</node>
+<node CREATED="1646488708130" FOLDED="true" ID="ID_1679136044" MODIFIED="1646488711304" POSITION="right" TEXT=" Appearance">
+<node CREATED="1646488711540" ID="ID_1094751687" MODIFIED="1646488715848" TEXT=" Prevalent in our society"/>
+<node CREATED="1646488716521" ID="ID_305341785" MODIFIED="1646488724063" TEXT="Always compare ourselves to others"/>
+<node CREATED="1646488724875" ID="ID_343297496" MODIFIED="1646488735176" TEXT="Judgement of how you look compared to something else"/>
+</node>
+<node CREATED="1646488975443" ID="ID_105197091" MODIFIED="1646488980848" POSITION="right" TEXT=" It&apos;s not about perfect, it&apos;s about helping"/>
+<node CREATED="1646489051259" FOLDED="true" ID="ID_1466304304" MODIFIED="1646489056767" POSITION="right" TEXT=" Put your defaults and identity further">
+<node CREATED="1646489057147" ID="ID_1924302404" MODIFIED="1646489062639" TEXT="It&apos;s how people will like you"/>
+<node CREATED="1646489063187" ID="ID_1688468157" MODIFIED="1646489073007" TEXT=" People want to be around weird people"/>
+<node CREATED="1646489073243" ID="ID_531967291" MODIFIED="1646489076743" TEXT=" ... who assume themselves"/>
+</node>
+<node CREATED="1646489151571" FOLDED="true" ID="ID_174185044" MODIFIED="1646489156959" POSITION="right" TEXT=" Make forward motions">
+<node CREATED="1646489157411" ID="ID_1754682920" MODIFIED="1646489167623" TEXT="Don&apos;t be stuck because you don&apos;t feel perfect"/>
+<node CREATED="1646489435866" ID="ID_358691733" MODIFIED="1646489438566" TEXT="You act anyway">
+<node CREATED="1646489464425" ID="ID_331970824" MODIFIED="1646489473766" TEXT="Don&apos;t spend too much time in preparation"/>
+<node CREATED="1646489474968" ID="ID_555181628" MODIFIED="1646489575407" TEXT="Iterate through and improve overtime"/>
+</node>
+</node>
+<node CREATED="1646489602641" ID="ID_179776145" MODIFIED="1646489609989" POSITION="right" TEXT=" We&apos;re all afraid"/>
+</node>
+</map>
